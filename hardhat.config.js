149a1dfd7b0c8de343f6ac0d42f881ae74aaa9f1
setup <vivonasg@gmail.com>
2022-12-24 13:49:04 -0600

    added evm ink and cosmowasm change

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  paths: {
+    sources: "./contracts/evm",
+    sources: "./contracts/evm/contracts",
+    artifacts: "./contracts/evm/artifacts"
+
+  }
+
 };
