f53e0048115793cb59f6bb916de80a225f9fde25
setup <vivonasg@gmail.com>
2023-02-05 19:15:50 -0600

    ola

--- docs/tutorials/ray_tutorial.py
+++ docs/tutorials/ray_tutorial.py
@@ -2,17 +2,27 @@ import commune
 
 
 class ModuleF:
-    wrapped_module=True
-    def bro(self, fam:str, bro: dict):
+    def __init__(self,bro='bro'):
+        self.bro = bro  
+    def bro(self, fam:str):
         return 'bro'
     
-commune.ray_init()
+class ModuleG:
+    def __init__(self,bro='bro'):
+        self.bro = bro  
+    def bro(self, fam:str):
+        return self.bro
+    
+# # print(commune.ray_actors())
+agentF = commune.module(ModuleF)
+agentG = commune.module(ModuleG)
+print(agentF('bro').bro)
+print(agentG('fam').bro)
 
+# print(agent.connect('ModuleF').bro('fam'))
+
+# # list actors
 # print(commune.ray_actors())
-# agent = commune.module(ModuleF(), return_class=True)
-
-# list actors
-print(commune.ray_actors())
 
 # you can also fetch the module from another file
 # In this example you are fetching the ModuleF from the root directory of commune
