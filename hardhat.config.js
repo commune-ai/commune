85aa4ad00cd48ded70e5c698117462481c7cf98e
setup <vivonasg@gmail.com>
2023-02-08 16:04:04 -0600

    web3 fixes with evm

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-let contract_path = './commune/web3/evm/data';
+let contract_path = './commune/web3/evm/contract/data';
 module.exports = {
   solidity: "0.8.9",
   paths: {
