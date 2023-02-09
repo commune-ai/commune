40880019db5c18ee0c829f23bde6a38bd2e8b3ee
setup <vivonasg@gmail.com>
2023-02-09 12:02:36 -0600

    docker compose

--- commune.sh
+++ commune.sh
@@ -130,15 +130,15 @@ while :; do
         --down)
             printf $COLOR_R'Doing a deep clean ...\n\n'$COLOR_RESET
             # eval docker network rm ${PROJECT_NAME}_default || true;
-            eval docker-compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
+            eval docker compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
             break;
         ;;
 
         
         --restart)
             printf $COLOR_R'Doing a deep clean ...\n\n'$COLOR_RESET
-            eval docker-compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
-            eval docker-compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME "$COMPOSE_FILES" up -d
+            eval docker compose --project-name=$PROJECT_NAME "$COMPOSE_FILES" down;
+            eval docker compose "$DOCKER_COMPOSE_EXTRA_OPTS" --project-name=$PROJECT_NAME "$COMPOSE_FILES" up -d
             break
             ;;
 
