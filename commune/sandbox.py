1b076c633062e9f09b12c124b6deacc5979f4fa5
salvivona <you@example.com>
2023-03-26 02:20:39 +0000

    deploying servers

--- commune/sandbox.py
+++ commune/sandbox.py
@@ -2,4 +2,27 @@ import commune
 import streamlit as st
 import torch
 
-st.write(commune.gpu_map())
+
+
+module = commune.get_module('model.transformer')
+
+st.write(list(module.shortcuts.keys()))
+
+models = [
+  "gpt2.7b",
+  "gpt3b",
+  "gptjt",
+  "gptjt_mod",
+  "gptj",
+  "gptj.pyg6b",
+  "gptj.instruct",
+  "gptj.codegen",
+  "gptj.hivemind",
+]
+
+for model in models:
+    model_kwargs =  {'model': model}
+    if 'gptj' in model:
+        model_kwargs['tokenizer'] = 'gptj'
+    module.launch(name=f'model.{model}',kwargs=model_kwargs, mode='pm2')
+
