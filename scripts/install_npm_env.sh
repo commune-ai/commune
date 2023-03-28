f0dde6781a4218b6c03aaccceaaf3e517b74d345
salvivona <vivonasg@gmail.com>
2023-03-28 23:37:59 +0000

    bro

--- scripts/install_npm_env.sh
+++ scripts/install_npm_env.sh
@@ -1,17 +1,32 @@
-#/bin/bash
+#!/bin/bash
 
-# install node
-export NODE_VERSION=16.17.1
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-export NVM_DIR=/root/.nvm
-. "$NVM_DIR/nvm.sh" && nvm install ${NODE_VERSION}
-. "$NVM_DIR/nvm.sh" && nvm use v${NODE_VERSION}
-. "$NVM_DIR/nvm.sh" && nvm alias default v${NODE_VERSION}
-export PATH="/root/.nvm/versions/node/v${NODE_VERSION}/bin/:${PATH}"
+# Check if Node.js is installed
+if ! command -v node &> /dev/null; then
+    echo "Node.js is not installed. Installing Node.js..."
+    
+    # Add NodeSource repository
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
-# Install node packages
-npm i -g pm2
-# npm install --save-dev hardhat
-# npm install --save-dev @nomicfoundation/hardhat-toolbox
-# npx hardhat
-# npm install @openzeppelin/contracts
+    # Install Node.js
+    sudo apt-get install -y nodejs
+else
+    echo "Node.js is already installed."
+fi
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null; then
+    echo "npm is not installed. Installing npm..."
+
+    # Install npm
+    sudo apt-get install -y npm
+else
+    echo "npm is already installed."
+fi
+
+# Verify the installation
+node -v
+npm -v
+
+# install pm2 
+npm install pm2 
+
--- scripts/install_python_env.sh
+++ scripts/install_python_env.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# This script installs the python environment for the commun project.
-
-if [[ "$1" == "venv" ]]; then
-    echo "Entering Virtual Environment (communenv)"
-    if [ -d "venv" ]; then
-        echo "communenv already exists, skipping, creating new one"
-    else
-        echo "communenv does not exist, creating new one"
-        python3 -m venv venv
-    fi
-
-    source communenv/bin/activate
-
-else
-    echo "Skipping Virtual Environment"
+# Check if python3-venv is installed
+if ! dpkg -s python3-venv &> /dev/null; then
+    echo "python3-venv package not found. Installing..."
+    sudo apt-get install python3-venv -y
 fi
 
+# Create a virtual environment
+python3 -m venv env
 
-# Upgrade pip
-
+# Activate the virtual environment
+source myenv/bin/activate
 pip install --upgrade pip
+
+# install bittensor
+pip install bittensor
+pip install https://github.com/opentensor/cubit/releases/download/v1.1.2/cubit-1.1.2-cp310-cp310-linux_x86_64.whl[test]
+
+# install commune
+pip install -e .
+
+# Deactivate the virtual environment
+echo "Setup is complete. To activate the virtual environment, run 'source myenv/bin/activate'"
+
+
 
 # Install dependencies
 
-pip install -e .
+
