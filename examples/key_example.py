4ac51a4ee3d76bf6a73e22b0a15fe6f408c9f799
salvivona <you@example.com>
2023-04-03 15:16:18 +0000

    ola

--- examples/key_example.py
+++ examples/key_example.py
@@ -1,40 +1,42 @@
-
+import streamlit as st
 import commune
 
-# you can create a key from any string
-key = commune.key(seed_hex='alice')
-key_class = key.__class__
+data = {'hey': 'whadup', 'hello': 'world'}
+seed = 'bro'
+# you can define a seed to generate the key
 
-# or you can generate it from a mnemonic
-mnemonic =  key_class.generate_mnemonic()
-key2 = commune.key(mnemonic=mnemonic)
-
-# if nothing is provided, a random key is generated (using the mnemonic method)
-key3 = commune.key()
-
-# for public only keys, just pass in the public key 
-key4 = key_class(ss58_address='5DkZvMMw5oPiy6UPs2Bi8AXzDQKMUwTsvJ52S97jimXnxGNP')
-
-# verify that the key is the same as the signture
-
-data_list = [{'bro': 'bro'}, 'whadup']
-import streamlit as st
-for data in data_list:
-    
-    signature : dict = key.sign(data=data, return_dict=True)
-    
-    
-    # verifity the signature
-    assert key.verify(signature) == True
-
-    # if we switch the public_key the verification will fail
-    signature['public_key'] = key2.public_key
-    assert key.verify(signature) == False
-
-# st.write(key4.params)
-# key2.set_params(**key3.params)
-# st.write(key2.load_state_dict(key4.state_dict(password='password'), password='password'))
-# st.write(key2.params)
+# if you dont provide anything, it will generate a random key
 
 
+# Encryption
+key = commune.key('bro')
 
+encrypted_data = key.encrypt(data)
+decrypted_data = key.decrypt(encrypted_data)
+st.write(decrypted_data)
+
+
+# Signing and Verification
+
+key = commune.key()
+
+# signatures can be strings 
+signature_str = key.sign(data)
+st.write(signature_str)
+signature_dict = key.sign(data, return_dict=True)
+st.write(signature_dict)
+
+# you can verify the signature with the public key
+
+assert key.verify(data, signature_str) == True
+
+# if you pass the signature_dict, it already has the data
+assert key.verify(signature_dict) == True
+
+# you can save the key and load it later
+key_state_dict = key.state_dict()
+old_key_address = key.ss58_address
+
+# you can load the key from the state dict
+key.load_state_dict(key_state_dict)
+st.write(old_key_address==key.ss58_address)
