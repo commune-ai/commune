b6f024b419cb5cb1998d8bddd820e6e3d044910a
salvivona <vivonasg@gmail.com>
2023-04-12 16:53:18 +0000

    olad

--- commune/__init__.py
+++ commune/__init__.py
@@ -11,7 +11,8 @@ config = Config
 # Substrate ss58_format
 __ss58_format__ = 42
 # set the module functions as globals
-for f in Module.get_class_methods():
-    globals()[f] = getattr(Module, f)
 for k,v in Module.__dict__.items():
     globals()[k] = v
+
+for f in Module.get_class_methods():
+    globals()[f] = getattr(Module, f)
