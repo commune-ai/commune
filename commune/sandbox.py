bd4b8060ebbc89054563ccb76cd6a4d6060c1cdd
salvivona <vivonasg@gmail.com>
2023-04-13 03:57:59 +0000

    ola

--- commune/sandbox.py
+++ commune/sandbox.py
@@ -1,3 +1,11 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import commune
-module = commune.connect('162.157.13.236:9251')
-print(module.key)
+model = 'EleutherAI/gpt-neo-125M'
+tokenizer = AutoTokenizer.from_pretrained(model)
+model = AutoModelForCausalLM.from_pretrained(model)
+model = commune.module(model)
+model.serve(name='gpt125m', wait_for_termination=False)
+commune.new_event_loop(True)
+model = commune.connect('gpt125m')
+
+print(model.functions())
