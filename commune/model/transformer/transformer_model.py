31a6d048ce699e081c1f67395fbc6bb5652e380f
Your Name <you@example.com>
2023-04-30 23:05:20 +0000

    bro

--- commune/model/transformer/transformer_model.py
+++ commune/model/transformer/transformer_model.py
@@ -137,6 +137,21 @@ class TransformerModel(Model):
         
         return loss
 
+
+    @classmethod
+    def generate(cls, text: str = 'hey whadup', max_length: int = 32, **kwargs):
+        model = cls.connect('model.gpt125m')
+        sample = model.tokenize(text)
+        responses = []
+        for i in range(max_length):
+            output = model.forward(**sample)
+            output['logits'] = cls.decode_topk(output['topk'])
+            output_ids = torch.argmax(output['logits'][:, -1, :], dim=-1)
+            cls.print(output_ids)
+            print(output_text)
+            responses.append(output_text)
+        
+        return self.forward(**sample)
     def _forward(self,  
                 input_ids: torch.Tensor, 
                 attention_mask: torch.Tensor = None,
@@ -440,6 +455,26 @@ class TransformerModel(Model):
         topk_values = probs.gather( index=topk_indices, dim=-1)
         encoded_probs = torch.cat([topk_values, topk_indices], dim=-1)  # [batch_size, sequence_len, topk + topk]
         return encoded_probs  # [batch_size, sequence_len, topk + topk]
+
+
+    @staticmethod
+    def decode_topk(  forward_response_tensor: torch.Tensor, topk:int=4096, vocab_size:int=50257) -> torch.Tensor:
+        """ Returns full logits by decoding topk-encoding input. """
+        batch_size, sequence_len, _ = forward_response_tensor.shape
+        encoded_probs = forward_response_tensor  # encoded probabilities: [batch_size, sequence_len, topk + topk]
+        topk_values = encoded_probs[..., :topk]  # topk probs: [batch_size, sequence_len, topk]
+        topk_indices = encoded_probs[..., topk:].long()  # topk probs indices: [batch_size, sequence_len, topk]
+
+        topk_pmass = topk_values.sum(dim=-1)  # topk probability mass: [batch_size, sequence_len]
+        remainder_pmass = torch.clamp(1 - topk_pmass, 1e-40, 1)  # remainder probability mass: [batch_size, sequence_len]
+        remainder_floor = remainder_pmass / (vocab_size - topk)  # divide remainder: [batch_size, sequence_len]
+
+        logits = torch.ones((batch_size, sequence_len, vocab_size), dtype=topk_values.dtype).to(topk_values.device)
+        logits *= torch.log(remainder_floor)[:, :, None]  # set probability floor: [batch_size, sequence_len, vocab_size]
+
+        logits.scatter_(-1, topk_indices, torch.log(topk_values + 1e-40))  # insert topk probs: [batch_size, sequence_len, vocab_size]
+
+        return logits  # [batch_size, sequence_len, vocab_size]
 
 
     def tokenizer_name(self):
