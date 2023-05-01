934a09133652706fa3483191335755681d90a587
salvivona <vivonasg@gmail.com>
2023-05-01 12:35:16 +0000

    bro

--- commune/model/model_dashboard.py
+++ commune/model/model_dashboard.py
@@ -1,42 +1,36 @@
 
+
+import commune
+commune.new_event_loop()
 import bittensor
+import streamlit as st
 print(bittensor.axon(netuid=3, port=8902))
 print(bittensor.subtensor())
 
-config = bittensor.neurons.core_server.neuron.config()
-config.neuron.no_set_weights = True
-config.neuron.no_autosave = True
-print(bittensor.neurons.core_server.neuron.config())
+# config = bittensor.neurons.core_server.neuron.config()
+# config.neuron.no_set_weights = True
+# config.neuron.no_autosave = True
+# print(bittensor.neurons.core_server.neuron.config())
 
 
-# import commune
-# import streamlit as st
+import commune
+import streamlit as st
+st.set_page_config(layout="wide")
 
-# @st.cache_data(persist=True)
-# def load_model_configs(model_prefix):
-#     commune.new_event_loop()
-#     models = commune.modules(model_prefix)
-#     model_configs = commune.call_pool(models, 'config')
-#     return model_configs
+@st.cache_data(persist=True)
+def load_model_configs(model_prefix):
+    commune.new_event_loop()
+    models = commune.modules(model_prefix)
+    model_configs = commune.call_pool(models, 'config')
+    return model_configs
 
-# model_prefix = 'model.gptj'
-# model_configs = load_model_configs(model_prefix)
-# models = list(model_configs.keys())
+model_prefix = st.text_input('search for a model...','model.gptj')
+model_configs = load_model_configs(model_prefix)
+models = list(model_configs.keys())
+import pandas as pd
 
-# with st.sidebar:
-#     models = st.multiselect('Models', models, models)
+models_df = pd.DataFrame([dict(model=model, **config.stats) for model, config in model_configs.items()])
+st.write(models_df)
 
 
-# for model, config in model_configs.items():
-#     with st.expander(model,True):
-#         config = commune.munch2dict(config)
-#         stats = config.pop('stats', None)
-#         epoch_loss_history = stats.pop('epoch_loss_history', None)
-#         st.write(epoch_loss_history)
-#         config = commune.flatten_dict(config)
-#         for metric, value in stats.items(): 
-#             if type(value) in [float, int, str]:
-#                 st.metric(label=metric, value=value)
-#             else:
-#                 st.write(f'{metric}')
-#                 st.write(value)
+
