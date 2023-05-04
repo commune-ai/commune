cacba47adf71dd873c4334fb61b1dc45c7d7fa65
salvivona <vivonasg@gmail.com>
2023-05-04 21:34:27 +0000

    bro

--- commune/huggingface/huggingface.py
+++ commune/huggingface/huggingface.py
@@ -12,11 +12,70 @@ import streamlit as st
 import torch
 from safetensors.torch import save_file, load_file
 
+
+shortcuts =  {
+    # 0-1B models
+    'gpt125m': 'EleutherAI/gpt-neo-125m',
+
+    # 1-3B models
+    'gpt2.7b': 'EleutherAI/gpt-neo-2.7B',
+    'gpt3b': 'EleutherAI/gpt-neo-2.7B',
+    'opt1.3b': 'facebook/opt-1.3b',
+    'opt2.7b': 'facebook/opt-2.7b',
+    # 'gpt3btuning' : ''
+
+    # 0-7B models
+    'gptjt': 'togethercomputer/GPT-JT-6B-v1',
+    'gptjt_mod': 'togethercomputer/GPT-JT-Moderation-6B',
+    'gptj': 'EleutherAI/gpt-j-6b',
+    'gptj.pyg6b': 'PygmalionAI/pygmalion-6b',
+    'gpt6b': 'cerebras/Cerebras-GPT-6.7B',
+    'gptj.instruct': 'nlpcloud/instruct-gpt-j-fp16',
+    'gptj.codegen': 'moyix/codegen-2B-mono-gptj',
+    'gptj.hivemind': 'hivemind/gpt-j-6B-8bit',
+    'gptj.adventure': 'KoboldAI/GPT-J-6B-Adventure',
+    'gptj.pygppo': 'TehVenom/GPT-J-Pyg_PPO-6B', 
+    'gptj.alpaca.gpt4': 'vicgalle/gpt-j-6B-alpaca-gpt4',
+    'gptj.alpaca': 'bertin-project/bertin-gpt-j-6B-alpaca',
+    'oa.galactia.6.7b': 'OpenAssistant/galactica-6.7b-finetuned',
+    'opt6.7b': 'facebook/opt-6.7b',
+    'llama': 'decapoda-research/llama-7b-hf',
+    'vicuna.13b': 'lmsys/vicuna-13b-delta-v0',
+    'vicuna.7b': 'lmsys/vicuna-7b-delta-v0',
+    'llama-trl': 'trl-lib/llama-7b-se-rl-peft',
+    'opt.nerybus': 'KoboldAI/OPT-6.7B-Nerybus-Mix',
+    'pygmalion-6b': 'PygmalionAI/pygmalion-6b',
+    # # > 7B models
+    'oa.pythia.12b': 'OpenAssistant/oasst-sft-1-pythia-12b',
+    'gptneox': 'EleutherAI/gpt-neox-20b',
+    'gpt20b': 'EleutherAI/gpt-neox-20b',
+    'opt13b': 'facebook/opt-13b',
+    'gpt13b': 'cerebras/Cerebras-GPT-13B',
+    
+        }
+
+
 class Huggingface(commune.Module):
     
+    shortcuts = shortcuts
     def __init__(self, config:dict=None):
         self.set_config(config)
         self.hf_api = HfApi(self.config.get('hub'))
+ 
+ 
+ 
+    def get_tokenizer(self, model_name_or_path:str=None, *args, **kwargs):
+        return AutoTokenizer.from_pretrained(model_name_or_path, *args, **kwargs)
+
+    def get_model(self, model_name_or_path:str=None, *args, **kwargs):
+        return AutoModel.from_pretrained(model_name_or_path, *args, **kwargs)
+ 
+ 
+    @classmethod
+    def resolve_model(cls, model:str, *args, **kwargs):
+        if model in cls.shortcuts:
+            model = cls.shortcuts[model]
+        return model
  
     def list_datasets(self,return_type = 'dict', filter_fn=lambda x: x['downloads'] > 1000, refresh_cache =False, *args, **kwargs):
         datasets = {} if refresh_cache else self.get_json('datasets',default={})
