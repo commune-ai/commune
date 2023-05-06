5606e57aea1343ce54934b89481bcbc96c1383bd
Your Name <you@example.com>
2023-05-06 02:54:05 +0000

    bro

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,4 @@
 import commune
-modules = commune.modules('module')
-commune.print(commune.call(modules, fn='ls'))
+modules = commune.modules('model.gptj::huck')
+model = commune.module('model.transformer')
+model.generate('wHAT IS THE POINT OF LIFE?', model='model.gptj::huck')
