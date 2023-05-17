91f4f9c29eb49c814cbcf7ee98c391a3ff66854b
salvivona <vivonasg@gmail.com>
2023-05-17 18:44:14 +0000

    bro

--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,5 @@
 import commune as c
 
-# bt = c.connect('bittensor')
-# c.print(bt.ss58())
+bt = c.module('bittensor')
+print(bt.get_metagraph())
 
-w = [
-    'miner::ensemble.0::local::3',
-    'miner::ensemble.10::local::3',
-    'miner::ensemble.12::local::3',
-    'miner::ensemble.13::local::3',
-    'miner::ensemble.14::local::3',
-    'miner::ensemble.15::local::3',
-    'miner::ensemble.1::local::3',
-    'miner::ensemble.2::local::3'
-]
-
-for _ in w:
-    c.kill(_)
