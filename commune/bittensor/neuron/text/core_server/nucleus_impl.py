44c8ce3e6c31171664e4827b2c310a12e488d05b
salvivona <vivonasg@gmail.com>
2023-05-17 18:43:44 +0000

    bro

--- commune/bittensor/neuron/text/core_server/nucleus_impl.py
+++ commune/bittensor/neuron/text/core_server/nucleus_impl.py
@@ -14,8 +14,8 @@ from bittensor.utils.tokenizer_utils import prep_tokenizer, get_translation_map,
     translate_special_token_text, pad_offsets, topk_token_phrases, compact_topk_token_phrases
 
 from loguru import logger; logger = logger.opt(colors=True)
-
-class server(torch.nn.Module):
+import commune as c
+class server(torch.nn.Module, c.Module):
     def __init__(self, 
                 config: 'bittensor.config' = None,
                 pretrained: bool = None,
@@ -53,11 +53,11 @@ class server(torch.nn.Module):
                 token_remap (:obj:Callable, `optional`):
                     Custom function that maps between tokenizers (defaults to self.remapping_token)
         """
-        super(server, self).__init__()
+        torch.nn.Module.__init__(self)
         if config == None: config = server.config()
-        self.config = config
+        self.config = config;print(config)
         self.std_tokenizer = bittensor.tokenizer()
-        self.device = 'cpu'
+        self.device = config.neuron.device
 
         #setting up pretrained model
         self.model_name = model_name if model_name != None else config.neuron.model_name
@@ -237,14 +237,14 @@ class server(torch.nn.Module):
                     Decoded predictions of the next token in the sentence.
 
         """
-        message, model_output, decoded_targets = self._forward(inputs, tokenizer)
+        message, model_output, decoded_targets = self.local_forward(inputs, tokenizer)
         shift_logits = decoded_targets[..., :-1, :].contiguous()
         shift_labels = inputs[..., 1:].contiguous()
         loss = self.loss_fct( shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1) )
 
         return loss, decoded_targets
 
-    def _forward(self, token_batch, tokenizer=None, encode_len=bittensor.__network_dim__, model_output = None):
+    def local_forward(self, token_batch, tokenizer=None, encode_len=bittensor.__network_dim__, model_output = None):
         r""" Forward pass through the pretrained model and possible mappings between hidden units.
              The response tensor should be the hidden units computed using the local context and
              with shape: [batch_size, sequence_len, __vocab_size__].
@@ -373,15 +373,15 @@ class server(torch.nn.Module):
             pre_logits = _model_output.logits  # [batch_size, sequence_len, self.tokenizer.vocab_len]
             probs_std = translate_logits_to_probs_std(pre_logits,
                                                       tokens['offset_mapping'], tokens['offset_mapping_std'],
-                                                      self.from_tokenizer, self.to_tokenizer,
+                                                      self.tokenizer, self.std_tokenizer,
                                                       self.split_map_cache,
                                                       self.to_translation_map, self.from_translation_map,
                                                       tokens['input_ids'], token_batch)
             probs_std = probs_std.to(self.device)
             logits_std = torch.log(probs_std + 1e-40)
             #removing the loss calculation for stablity testing
-            original_loss = self.get_loss_fct(pre_logits, tokens['input_ids'][:, -pre_logits.shape[1]:]).item()
-            translated_loss = self.get_loss_fct(logits_std, token_batch[:, -pre_logits.shape[1]:]).item()
+            original_loss = self.get_loss_fct(pre_logits, tokens['input_ids']).item()
+            translated_loss = self.get_loss_fct(logits_std, token_batch).item()
             message = f'Loss: {original_loss:.2f} → {translated_loss:.2f}'
             
             return message, _model_output, logits_std
@@ -434,7 +434,6 @@ class server(torch.nn.Module):
             std_tokenizer = self.std_tokenizer
 
         tokens = self.token_remap(token_batch, std_tokenizer)
-        
 
         def _forward(_model_output=model_output):
             if _model_output is None:
@@ -449,7 +448,7 @@ class server(torch.nn.Module):
             # then compact new token phrases and probabilities into 1-D tensor
             topk_tensor = topk_token_phrases(last_logits, self.tokenizer, topk=topk)  # [batch_size, (topk + 1), max_len]
 
-            original_loss = self.get_loss_fct(_model_output.logits, tokens['input_ids'][:, -_model_output.logits.shape[1]:]).item()
+            original_loss = self.get_loss_fct(_model_output.logits, tokens['input_ids']).item()
 
             message = f'Loss: {original_loss:.2f}'
 
@@ -474,11 +473,11 @@ class server(torch.nn.Module):
                 check_status (:type: `bool`):
                     True if the model_output is valid.
         """
-        # if hasattr(model_output, 'hidden_states') and model_output.hidden_states[-1].isnan().sum() > 0:
-        #     raise ValueError("Got nan value from model last hidden state. If you are using cuda with autocast, try remove setting --neuron.autocast.")
+        if hasattr(model_output, 'hidden_states') and model_output.hidden_states[-1].isnan().sum() > 0:
+            raise ValueError("Got nan value from model last hidden state. If you are using cuda with autocast, try remove setting --neuron.autocast.")
 
-        # if model_output.logits.isnan().sum() > 0:
-        #     raise ValueError("Got nan value from model logits. If you are using cuda with autocast, try remove setting --neuron.autocast.")
+        if model_output.logits.isnan().sum() > 0:
+            raise ValueError("Got nan value from model logits. If you are using cuda with autocast, try remove setting --neuron.autocast.")
 
         return True
 
--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,6 @@
 import commune as c
 
-# bt = c.connect('bittensor')
-# c.print(bt.ss58())
 
-w = [
-    'miner::ensemble.0::local::3',
-    'miner::ensemble.10::local::3',
-    'miner::ensemble.12::local::3',
-    'miner::ensemble.13::local::3',
-    'miner::ensemble.14::local::3',
-    'miner::ensemble.15::local::3',
-    'miner::ensemble.1::local::3',
-    'miner::ensemble.2::local::3'
-]
-
-for _ in w:
-    c.kill(_)
+server = c.import_object('commune.bittensor.neuron.text.server')
+server.serve()
+print(server)
