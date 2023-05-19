585b249eeb88410ba1bbe9ac65f7cc51a2abf863
Your Name <you@example.com>
2023-05-19 14:42:37 +0000

    bro

--- scripts/install_python_env.sh
+++ scripts/install_python_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # # Check if python3-venv is installed
-# if ! dpkg -s python3-venv &> /dev/null; then
-#     echo "python3-venv package not found. Installing..."
-#     sudo apt-get install python3-venv -y
-# fi
+if ! dpkg -s python3-pip &> /dev/null; then
+    echo "python3-venv package not found. Installing..."
+    sudo apt install python3-pip
+fi
 
 # # Create a virtual environment
 # python3 -m venv env
