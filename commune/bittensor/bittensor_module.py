e19584024e5b502318d0d039d5fe2380e34be000
salvivona <vivonasg@gmail.com>
2023-05-19 19:59:09 +0000

    working checkpoint

--- commune/bittensor/bittensor_module.py
+++ commune/bittensor/bittensor_module.py
@@ -731,7 +731,7 @@ class BittensorModule(c.Module):
            
            
     @classmethod
-    def add_keys(cls, name='commune',
+    def add_keys(cls, name=default_coldkey,
                       hotkeys=[i+1 for i in range(16)] , 
                       use_password: bool=False,
                       overwrite:bool = False):
@@ -1015,7 +1015,7 @@ class BittensorModule(c.Module):
         
         self.streamlit_neuron_metrics()
     @classmethod
-    def balance(cls, wallet='commune'):
+    def balance(cls, wallet=default_coldkey):
         wallet = cls.get_wallet(wallet)
         return wallet.balance 
     
@@ -1201,7 +1201,7 @@ class BittensorModule(c.Module):
     # add_server = deploy_server
     
     @classmethod
-    def neuron(cls, *args, mode='commune', **kwargs):
+    def neuron(cls, *args, mode=default_coldkey, **kwargs):
         if mode == 'vanilla':
             return bittensor.neurons.core_server.neuron(*args, **kwargs)
         else:
@@ -1659,13 +1659,61 @@ class BittensorModule(c.Module):
     
     @classmethod
     def sandbox(cls):
-        for wallet in cls.wallets('commune'):
+        for wallet in cls.wallets(default_coldkey):
             if '.Hot' not in wallet:
                 ck, hk = wallet.split('.')
                 cls.rename_wallet(wallet, f'{ck}.Hot{hk}')
     @classmethod
     def local_node(cls):
         return cls.cmd('sudo docker-compose up -d', cwd=f'{cls.repo_path}/subtensor', verbose=True)
+    
+    
+
+    shortcuts =  {
+        # 0-1B models
+        'gpt125m': 'EleutherAI/gpt-neo-125m',
+
+        # 1-3B models
+        'gpt2.7b': 'EleutherAI/gpt-neo-2.7B',
+        'gpt3b': 'EleutherAI/gpt-neo-2.7B',
+        'opt1.3b': 'facebook/opt-1.3b',
+        'opt2.7b': 'facebook/opt-2.7b',
+        # 'gpt3btuning' : ''
+
+        # 0-7B models
+        'gptjt': 'togethercomputer/GPT-JT-6B-v1',
+        'gptjt_mod': 'togethercomputer/GPT-JT-Moderation-6B',
+        'gptj': 'EleutherAI/gpt-j-6b',
+        'gptj.pyg6b': 'PygmalionAI/pygmalion-6b',
+        'gpt6b': 'cerebras/Cerebras-GPT-6.7B',
+        'gptj.instruct': 'nlpcloud/instruct-gpt-j-fp16',
+        'gptj.codegen': 'moyix/codegen-2B-mono-gptj',
+        'gptj.hivemind': 'hivemind/gpt-j-6B-8bit',
+        'gptj.adventure': 'KoboldAI/GPT-J-6B-Adventure',
+        'gptj.pygppo': 'TehVenom/GPT-J-Pyg_PPO-6B', 
+        'gptj.alpaca.gpt4': 'vicgalle/gpt-j-6B-alpaca-gpt4',
+        'gptj.alpaca': 'bertin-project/bertin-gpt-j-6B-alpaca',
+        'oa.galactia.6.7b': 'OpenAssistant/galactica-6.7b-finetuned',
+        'opt6.7b': 'facebook/opt-6.7b',
+        'llama': 'decapoda-research/llama-7b-hf',
+        'vicuna.13b': 'lmsys/vicuna-13b-delta-v0',
+        'vicuna.7b': 'lmsys/vicuna-7b-delta-v0',
+        'llama-trl': 'trl-lib/llama-7b-se-rl-peft',
+        'opt.nerybus': 'KoboldAI/OPT-6.7B-Nerybus-Mix',
+        'pygmalion-6b': 'PygmalionAI/pygmalion-6b',
+        # # > 7B models
+        'oa.pythia.12b': 'OpenAssistant/oasst-sft-1-pythia-12b',
+        'gptneox': 'EleutherAI/gpt-neox-20b',
+        'gpt20b': 'EleutherAI/gpt-neox-20b',
+        'opt13b': 'facebook/opt-13b',
+        'gpt13b': 'cerebras/Cerebras-GPT-13B',
+        'gptjvr': os.path.expanduser('~/models/gpt-j-6B-vR'),
+        'stablellm7b': 'StabilityAI/stablelm-tuned-alpha-7b',
+        'fish': os.path.expanduser('~/fish_model'),
+        
+            }
+
+
 if __name__ == "__main__":
     BittensorModule.run()
 
