1d3bc87676003f247a7abbee13b56062e611f4c4
Luca Vivona <lucavivona01@gmail.com>
2023-06-02 23:11:37 -0400

    [PENDING] seeing what to expand, and build

--- commune/store/vector/vector_store.py
+++ commune/store/vector/vector_store.py
@@ -1,7 +1,13 @@
 import commune as c
 import torch
+import numpy as np
+from typing import List, Union, Literal
+from safetensors import safe_open
+from safetensors.torch import save_file
+
 
 class VectorStore(c.Module):
+    """A class for storing vectors and searching them using vector similarity metrics"""
     def __init__(self, 
                     config = None
                  ):
@@ -22,25 +28,25 @@ class VectorStore(c.Module):
         return c.deploy()
         
     def encode(self, text:str, **kwargs):
+        """Encodes a text into a vector representation"""
         return self.model.embed(text, **kwargs)
     
 
     def resolve_vector(self, v:torch.Tensor):
-        
+        """Converts a vector to a torch.Tensor and checks that it has the same shape as the example vector"""
         if isinstance(v, list):
             v = torch.tensor(v)
         elif isinstance(v, np.ndarray):
             v = torch.from_numpy(v)
             
         assert isinstance(v, torch.Tensor), f'Expected a torch.Tensor, got {type(v)}'
-        if self.example_vector == None:
-            self.example_vector = v
-        assert self.example_vector.shape == v.shape, f'Expected a vector of shape {self.example_vector.shape}, got {v.shape}'
-        
+        if len(v.shape) == 1:
+            v = v.unsqueeze(0)
         return v
     
-    @classmthod
+    @classmethod
     def resolve_model(self, model=None):
+        """Converts a model to a torch.Tensor and checks that it has the same shape as the example vector"""
         if model == None:
             model = self.model
         elif isinstance(model, str):
@@ -49,28 +55,28 @@ class VectorStore(c.Module):
         return model
         
 
-    def embed(self, text:str, model=None **kwargs):
+    def embed(self, text:str, model=None, **kwargs):
         model = self.resolve_model(model)
         return self.model.embed(text, **kwargs)
     
-    def add_vector(self, k, v , verbose=True):
+    def append(self, k, v, verbose=True):
     
-
         v = self.resolve_vector(v)
         stacked_vectors = []
         if len(self.vectors) == 0:
             stacked_vectors = [v]
         else:
             stacked_vectors = [self.vectors, v]
+            
         
-        self.vectors = torch.stack(stacked_vectors)
+        self.vectors = torch.cat(stacked_vectors, dim=0)
         idx = len(self.vectors) - 1
         if verbose:
             c.print(f'Adding vector {k} at index {idx}, {self.vectors}')
         self.k2index[k] = idx
         self.index2k[idx] = k
         
-    def rm_vector(self, k):
+    def pop(self, k):
         last_idx = len(self.vectors) - 1
         print(f'last_idx: {last_idx}')
         # swap the vector to be removed with the last vector
@@ -87,8 +93,6 @@ class VectorStore(c.Module):
         self.vectors = self.vectors[:-1]
 
 
-
-
     def search(self, query, top_k=10, chunks=1):
         assert len(self.vectors) > 0, 'No vectors stored in the vector store'
         c.print(f'query: {query}', 'vector shape:', self.vectors.shape)
@@ -96,7 +100,7 @@ class VectorStore(c.Module):
         if len(query_vector.shape) == 1:
             query_vector = query_vector.unsqueeze(0)
         # Convert the query into a tensor (assuming it's a vector representation)
-        print(query_vector.shape, self.vectors.shape)
+
         similarities = torch.einsum('ij,kj->i', query_vector, self.vectors)  # Compute the similarity scores between the query and stored vectors
         indices = similarities.argsort(descending=True)[:top_k]  # Sort the similarity scores and retrieve the indices of the top-k results
         results = {}  # Create an empty dictionary to store the results
@@ -107,15 +111,40 @@ class VectorStore(c.Module):
             results[key] = score  # Store the key-score pair in the results dictionary
         return results  # Return the dictionary of results
 
+    def cosine_similarity(self, v1 : Union[list, np.ndarray, torch.Tensor], v2 : Union[list, np.ndarray, torch.Tensor]):
+        return torch.dot(v1, v2) / (torch.norm(v1) * torch.norm(v2))
 
     @classmethod
-    def test(cls):
+    def manhattan_distance(cls, v1 : Union[list, np.ndarray, torch.Tensor], v2 : Union[list, np.ndarray, torch.Tensor]):
+        return torch.sum(torch.abs(v1 - v2))
+
+    @classmethod
+    def save(cls, name : str='memory', method : Literal["safetensors", "pickle", "torch"]='safetensors'):
         self = cls()
-        self.add_vector('test', [1,2,3])
-        assert self.search([1,2,3]) == {'test': 14.0}
-        self.rm_vector('test')
-        assert len(self.vectors) == 0
-        print('test passed')
+        dir = f"{self.dirpath()}/"
         
+        if method == 'safetensors':
+            file = f'{name}.safetensors'
+            dir += file
+            mem : dict = {f"{name}" : torch.ones((5,100, 100))}
+            save_file(mem, dir); self.putc(name , file)
+
+        elif method == 'pickle':
+            file = f'{name}.pickle'
+            # save_file(self.vectors, file)
         
-        
+
+    @classmethod
+    def load(cls, key : str="memory" , method : Literal["safetensors", "pickle", "torch"]='safetensors', *args, **kwargs):
+        # FIX ME DOES NOT WORK
+        self = cls()
+        path = f"{self.dirpath()}/{self.getc(key)}"
+        tensors = {}
+        if method == 'safetensors':
+            with safe_open(path, *args, **kwargs) as f:
+                for keys in f.keys():
+                    tensors[keys] = f.get_tensor(keys)
+        return tensors
+
+    def __repr__(self):
+        return f'VectorStore with {len(self.vectors)} vectors'
