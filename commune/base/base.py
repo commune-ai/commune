719e68c49214384ffa85cbfa9c8b0d192b85822a
salvivona <vivonasg@gmail.com>
2023-06-04 16:08:39 +0000

    ola

--- commune/base/base.py
+++ commune/base/base.py
@@ -3,36 +3,8 @@ import commune
 class Base(commune.Module):
     def __init__(self, **kwargs):
         config = self.set_config(config=kwargs)
-        
-    @property
-    def users(self):
-        return self.config.users
-        
-
-    
-    def add_user(self, user: str, password: str=None) -> None:
-        self.config.users.append(user)
-        return self.config.users[-1]
-        
-    def rm_user(self, user: str) -> None:
-        for i in range(len(self.config.users)):
-            if self.config.users[i] == user:
-                del self.config.users[i]
-                
-                
-    def save(self):
-        self.put('config',self.config)
-        
-    def load(self):
-        self.config = self.get('config')
-    
-    @classmethod
-    def test(cls):
-        cls()
-    def users(self):
-        return self.config.users
-    
-
+    def run(self):
+        print('Base run')
     
 if __name__ == '__main__':
     Base.run()
