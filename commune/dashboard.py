d645b8ff190688d0113a0bc8b9a5737c5e92bbf9
salvivona <vivonasg@gmail.com>
2023-06-07 17:25:34 +0000

    bro

--- commune/dashboard.py
+++ commune/dashboard.py
@@ -24,7 +24,7 @@ class Dashboard(commune.Module):
 
         module_list =  self.module_list
         
-        st.write(self.module_config)
+
         
         
         st.write(f'## Module: {self.module_name}')
@@ -44,6 +44,10 @@ class Dashboard(commune.Module):
         launch_col = cols[0]
         kwargs_cols = cols[2:]
         
+        
+        # with st.expander('Config'):
+        #     st.write(self.module_config)
+        
         with launch_col:
        
             name = st.text_input('**Name**', self.module_name) 
@@ -51,13 +55,12 @@ class Dashboard(commune.Module):
             # tag = st.text_input('**Tag**', 'None')
             if tag == 'None' or tag == '' or tag == 'null':
                 tag = None
-            refresh = st.checkbox('**Refresh**', False)
+            # refresh = st.checkbox('**Refresh**', False)
             # mode = st.selectbox('**Select Mode**', ['pm2',  'ray', 'local'] ) 
             mode = 'pm2'
             serve = True
             launch_button = st.button('Launch Module')  
             
-        st.write(fn_info)
             
         fn_info['default'].pop('self', None)
         fn_info['default'].pop('cls', None)
@@ -113,7 +116,7 @@ class Dashboard(commune.Module):
                 refresh = True,
                 kwargs = kwargs,
             )
-            commune.launch(**launch_kwargs)
+            bro = commune.launch(**launch_kwargs)
 
             
 
@@ -126,14 +129,9 @@ class Dashboard(commune.Module):
             self.module_config = self.module.config()
 
 
-            self.streamlit_peers()
-
-            with st.expander('Module Tree'):
-                st.write(self.module_list)
-            with st.expander('Servers'):
-                st.write(self.servers)
-            with st.expander('Peers'):
-                st.write(self.peers())
+            with st.expander('My Modules'):
+                st.write(commune.namespace())
+                # st.write(self.servers)
             
             self.update_button = st.button('Update', False)
 
@@ -166,96 +164,10 @@ class Dashboard(commune.Module):
             with st.expander(peer_name, True):
                 peer_info['address']=  f'{peer_info["ip"]}:{peer_info["port"]}'
                 st.write(peer_info)
-                
-            
-            
-        
-        
      
 
         peer_info_map = {}
- 
-    def function_call(self, module:str, fn_name: str = '__init__' ):
-        
-        module = commune.connect(module)
-        peer_info = module.peer_info()
-        
-        function_info_map = self.module.function_info_map()
-        fn_name = fn_name
-        fn_info = function_info_map[fn_name]
-        
-        kwargs = {}
-        cols = st.columns([3,1,6])
-        cols[0].write(f'#### {name}.{fn_name}')
-        cols[2].write('#### Module Arguments')
-        cols = st.columns([2,1,4,4])
-        launch_col = cols[0]
-        kwargs_cols = cols[2:]
-        st.write(function_info_map)
-                
-        st.write(self.module_config, 'brooo')
-        
-        with launch_col:
-            name = st.text_input('**Name**', self.module_name) 
-            tag = None
-            # tag = st.text_input('**Tag**', 'None')
-            if tag == 'None' or tag == '' or tag == 'null':
-                tag = None
-            # mode = st.selectbox('**Select Mode**', ['pm2',  'ray', 'local'] ) 
-            mode = 'pm2'
-            serve = True
-            launch_button = st.button('Launch Module')  
-            
-        
-        # kwargs_cols[0].write('## Module Arguments')
-        for i, (k,v) in enumerate(fn_info['default'].items()):
-            
-            optional = fn_info['default'][k] != 'NA'
-            fn_key = k 
-            if k in fn_info['schema']:
-                k_type = fn_info['schema'][k]
-                if k_type.startswith('typing'):
-                    k_type = k_type.split('.')[-1]
-                fn_key = f'**{k} ({k_type}){"" if optional else "(REQUIRED)"}**'
-            kwargs_col_idx  = i 
-            if k in ['kwargs', 'args'] and v == 'NA':
-                continue
-            
-            
-            
-            kwargs_col_idx = kwargs_col_idx % (len(kwargs_cols))
-            init_kwarg[k] = kwargs_cols[kwargs_col_idx].text_input(fn_key, v)
-            
-        init_kwarg.pop('self', None )
-        init_kwarg.pop('cls', None)
-        
-
-        if launch_button:
-            kwargs = {}
-            for k,v in init_kwarg.items():
-                if v == 'None':
-                    v = None
-                elif k in fn_info['schema'] and fn_info['schema'][k] == 'str':
-                    v = v
-                elif k == 'kwargs':
-                    continue
-                elif v == 'NA':
-                    assert k != 'NA', f'Key {k} not in default'
-                else:
-                    v = eval(v) 
-                
-                kwargs[k] = v
-                
-            launch_kwargs = dict(
-                module = self.module,
-                name = name,
-                tag = tag,
-                mode = mode,
-                refresh = refresh,
-                kwargs = kwargs,
-            )
-            commune.launch(**launch_kwargs)
-            
+    
    
     def streamlit_playground(self):
         class bro:
@@ -278,6 +190,7 @@ class Dashboard(commune.Module):
         self.streamlit_sidebar()
 
         self.streamlit_module_browser()
+        self.streamlit_peers()
 
 
         
