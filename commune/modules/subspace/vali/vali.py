9384f7def8e62cb1b18ada436c9f7575d470f936
Your Name <you@example.com>
2023-06-30 13:59:38 +0000

    vali updates

--- commune/modules/subspace/vali/vali.py
+++ commune/modules/subspace/vali/vali.py
@@ -8,19 +8,13 @@ c.new_event_loop()
 class Validator(c.Module):
 
     def __init__(self, config=None,
-                 run:bool = False,
                  **kwargs):
         self.set_config(config=config, kwargs=locals())
-        self.subspace = c.module('subspace')()
-        self.state = self.subspace.state_dict()
-        self.modules = self.state['modules']
-        self.namespace = self.state['namespace']
-        self.state = self.config.get('state', self.state)
-        self.module_info = {}
-        self.response_history = {}
-        self.w = {}
-        self.count = 0
-        if run:
+        self.subspace = c.module(self.config.network)()
+        self.modules = self.subspace.modules()
+        if self.config.load == True:
+            self.load(self.config.tag)
+        if self.config.run:
             self.run()
         
         
@@ -33,17 +27,17 @@ class Validator(c.Module):
     
          
     def eval_module(self, module=None, verbose:bool=True):
-        module = c.choice(self.modules) if module == None else None
+        module_state = c.choice(self.modules) if module == None else None
         w = 1
         error = None
         try:
-            module_name = module['name']
+            module_name = module_state['name']
             # get connection
-            module = c.connect(module['address'], network=self.config.network, timeout=self.config.timeout)
+            module = c.connect(module_state['address'], network=self.config.network, timeout=self.config.timeout)
             
             # get info
-            if module_name not in self.module_info:
-                self.module_info[module_name] = module.info(timeout=self.config.timeout)
+            if module_name not in module_state:
+                module_state = module.info(timeout=self.config.timeout)
         except Exception as e:
             # something went wrong, set score to 0, 
             w = 0
@@ -52,31 +46,33 @@ class Validator(c.Module):
         w = self.get_score(module) if w != 0 else 0
 
         response = {'module': module_name, 'w': w, 'error': error}
-        
-        self.count += 1
         if verbose:
             c.print(response, color='white')
             
-        self.register_response(response)
-            
-       
-    def register_response(self, response:dict):
+        self.count += 1
         w = response['w']
         # we want to mix more recent scores with older ones
         w = w * self.config.alpha + (1-self.config.alpha) * self.w.get(module_name, 0)
         self.w[module_name] = w
         if self.count % self.config.save_interval = 0:
-            self.put('w', self.w)
-        if module_name not in self.response_history:
-            self.response_history[module_name] = []
-        self.response_history[module_name].append(response)
-    
+            self.save()
+        if module_name not in module_state:
+            module_state[module_name] = []
+        module_state[module_name].append(response)
+        module_id = module_state['uid']
+        self.modules[module_id] = module_state
+        c.print(f'w: {w}')
 
     def save(self, tag=None):
-        tag = self.config.tag if tag == None else tag
         
+        
+        tag = self.config.tag if tag == None else tag
+        for m in self.modules:
+            
+            
         self.put(f'{tag}/w', self.w)
-         self.put(f'{tag}/response_history', self.response_history)
+        self.put(f'{tag}/response_history', self.response_history)
+        self.put(f'{tag}/network_state', self.response_history)
 
     def run(self):
         
@@ -95,6 +91,6 @@ class Validator(c.Module):
         self.running = False
         
     @classmethod
-    def test(cls):
-        self = cls()
+    def test(cls, **kwargs):
+        self = cls(**kwargs)
         
