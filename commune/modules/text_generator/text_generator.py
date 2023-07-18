64e0e62ac362f983c3e6023ee14183147d83efc6
Your Name <you@example.com>
2023-07-18 23:35:00 +0000

    text generator

--- commune/modules/text_generator/text_generator.py
+++ commune/modules/text_generator/text_generator.py
@@ -3,19 +3,15 @@ from text_generation import Client
 class TextGenerator(c.Module):
     image = 'text_generator'
     
-    def serve(self,  name=None,
-                    model = None,
-                    tag = None,
-                    image=image,
+    def serve(self,tag = None,
                     num_shard=2, 
-                    gpu='all',
+                    gpus='all',
                     shm_size='100g',
                     volume=None, 
                     build:bool = True,
                     port=None):
-        if model == None:
-            model = self.config.model
-        name =  image_tag +"_"+ model
+        model = self.config.model
+        name =  self.image +"_"+ model
 
         if tag != None:
             name = f'{name}_{tag}'
@@ -23,76 +19,97 @@ class TextGenerator(c.Module):
 
         model_id = self.config.shortcuts.get(model, model)
         
-         
-        port = c.resolve_port(port)
+        if port == None:
+            port = c.resolve_port(port)
 
         if volume == None:
             volume = self.resolve_path('data')
             c.mkdir(volume)
         # if build:
-        #     cls.build(tag=tag)
+        #     self.build(tag=tag)
         cmd_args = f'--num-shard {num_shard} --model-id {model_id}'
-        cmd = f'docker run -d --gpus {gpu} --shm-size {shm_size} -p {port}:80 -v {volume}:/data --name {name} {image} {cmd_args}'
+        cmd = f'docker run -d --gpus \'"device={gpus}"\' --shm-size {shm_size} -p {port}:80 -v {volume}:/data --name {name} {self.image} {cmd_args}'
 
         output_text = c.cmd(cmd, sudo=True, output_text=True)
-        c.print('WHADUP',str(output_text))
         if 'Conflict. The container name' in output_text:
             c.print(f'container {name} already exists, restarting...')
             contianer_id = output_text.split('by container "')[-1].split('". You')[0].strip()
             c.cmd(f'docker rm -f {contianer_id}', sudo=True, verbose=True)
             c.cmd(cmd, sudo=True, verbose=True)
+    # def fleet(self, num_shards = 2, buffer=5_000_000_000, **kwargs):
+    #     model_size = c.get_model_size(self.config.model)
+    #     model_shard_size = (model_size // num_shards ) + buffer
+    #     max_gpu_memory = c.max_gpu_memory(model_size)
+    #     c.print(max_gpu_memory, model_shard_size)
 
 
-    @classmethod
-    def build(cls, image=image):
-        cmd = f'sudo docker build -t {image} .'
-        c.cmd(cmd, cwd=cls.dirpath(), verbose=True)
+    #     c.print(gpus)
 
 
-    @classmethod
-    def namespace(cls):
+        
+        
+
+    def build(self):
+        cmd = f'sudo docker build -t {self.image} .'
+        c.cmd(cmd, cwd=self.dirpath(), verbose=True)
+
+    def logs(self, name):
+        return c.cmd(f'sudo docker logs {name}', cwd=self.dirpath())
+
+
+    def namespace(self):
         output_text = c.cmd('sudo docker ps')
         names = [l.split('  ')[-1].strip() for l in output_text.split('\n')[1:-1]]
         addresses = [l.split('  ')[-2].split('->')[0].strip() for l in output_text.split('\n')[1:-1]]
-        namespace = {k:v for k,v in  dict(zip(names, addresses)).items() if k.startswith(cls.image)}
+        namespace = {k:v for k,v in  dict(zip(names, addresses)).items() if k.startswith(self.image)}
         return namespace
 
-    @classmethod
-    def servers(cls):
-        return list(cls.namespace().keys())
-    @classmethod
-    def addresses(cls):
-        return list(cls.namespace().values())
     
-    @classmethod
-    def random_server(cls):
-        return c.choice(cls.servers())
-    @classmethod
-    def random_address(cls):
-        return c.choice(cls.addresses())
+    def servers(self):
+        return list(self.namespace().keys())
+    
+    def addresses(self):
+        return list(self.namespace().values())
+    
+    def random_server(self):
+        return c.choice(self.servers())
+    
+    def random_address(self):
+        return c.choice(self.addresses())
+
+    
+    def install(self):
+        c.cmd('pip install -e clients/python/', cwd=self.dirpath(), verbose=True)
+
 
     @classmethod
-    def install(cls):
-        c.cmd('pip install -e clients/python/', cwd=cls.dirpath(), verbose=True)
-
-
-    def generate(self, 
+    def generate(cls, 
                 prompt = 'what is up', 
                 max_new_tokens:int=100, 
                 model:str = None, 
+                timeout = 6,
                 **kwargs):
+
+        self = cls()
                 
-        if model == None:
-            model = self.random_server()
+        if model != None:
             address = self.namespace()[model]
+        else:
+            address = self.random_address()
+        c.print(f'generating from {address}')
 
         client = Client('http://'+address)
         generated_text = client.generate_stream(prompt, max_new_tokens=max_new_tokens, **kwargs)
         output_text = ''
+
+        start_time = c.time()
         for text_obj in generated_text:
+            if c.time() - start_time > timeout:
+                break
             text =  text_obj.token.text
             output_text += text
             c.print(text, end='')
+
 
         return output_text
 
