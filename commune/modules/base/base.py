a539005cc8613042d06b7250ca99f47fa78d9588
Your Name <you@example.com>
2023-07-24 18:32:37 +0000

    fml

--- commune/modules/base/base.py
+++ commune/modules/base/base.py
@@ -7,21 +7,3 @@ class Base(c.Module):
         print('Base run')
 
 
-"""
-import freeGPT
-import asyncio
-
-def chat(prompt):
-    async def main():
-        try:
-            resp = await getattr(freeGPT, "gpt3").Completion.create(prompt)
-            return resp
-        except Exception as e:
-            return str(e)
-    return asyncio.get_event_loop().run_until_complete(main())
-
-print(chat("hey!"))
-"""
-
-
-# Write a module based on the Base Module 
