92db2d52e42e056cbac63bd8e0e26d673e0571b5
Your Name <you@example.com>
2023-07-24 14:54:17 +0000

    refactor

--- commune/module/module.py
+++ commune/module/module.py
@@ -2462,55 +2462,42 @@ class c:
               name:str=None, 
               tag:str=None,
               # networking 
-              address:str = None,
               ip:str=None, 
               port:int=None ,
-              key = None, # key for server's identity
               refresh:bool = True, # refreshes the server's key
               whitelist:List[str] = None, # list of addresses that can connect to the server
               blacklist:List[str] = None, # list of addresses that cannot connect to the server
               wait_for_termination:bool = True, # waits for the server to terminate before returning
               wait_for_server:bool = False, # waits for the server to start before returning
-              wait_for_server_timeout:int = 30, # timeout for waiting for the server to start
-              wait_for_server_sleep_interval: int = 1, # sleep interval for waiting for the server to start
               verbose:bool = False, # prints out information about the server
-              reserve_port:bool = False, # reserves the port for the server
-              tag_seperator: str = '::', # seperator for the tag
               remote:bool = True, # runs the server remotely (pm2, ray)
               args:list = None,  # args for the module
               kwargs:dict = None,  # kwargs for the module
               update: bool = True,
               mode:str = server_mode,
+              key = None, # key for server's identity
+
               
               ):
         '''
         
         Servers the module on a specified port
         '''
-        if update:
-            c.update()
         kwargs  = kwargs if kwargs else {}
         args = args if args else []
         name = cls.resolve_server_name(module=module, name=name, tag=tag)
-        tag = None
+
         if remote:
             remote_kwargs = cls.locals2kwargs(locals(), merge_kwargs=False)
             remote_kwargs['remote'] = False
             return cls.remote_fn('serve', name=name, kwargs=remote_kwargs, )
         
-        if address != None and port == None:
-            port = int(address.split(':')[-1])
-        # ensure the port is free
-        if port == None:
-            port = cls.free_port(reserve=reserve_port)
+        if update:
+            c.update()
 
-        port = int(port)
-    
-        module = cls.resolve_module(module)
-
-        self = module(*args, **kwargs)
-
-
+        self = cls.resolve_module(module)(*args, **kwargs)
+        port = c.resolve_port(port)
+        
         if whitelist == None:
             whitelist = self.whitelist
         if blacklist == None:
@@ -2523,18 +2510,17 @@ class c:
                     c.print(f'Stopping existing server {name}', color='yellow')
                 c.kill(name)
             else: 
+                
                 raise Exception(f'The server {name} already exists')
-
-        # ensure that the module has a name
-        for k in ['module_name', 'module_id', 'my_name', 'el_namo', 'name']:
-            if k not in self.__dict__:
-                self.__dict__[k] = name
-
-        module_class = c.module(f'server.{mode}')
-        c.print(f'Serving {name} on port {port}', color='yellow')
-        server = module_class(ip=ip, port=port,module = self,name= name,
+        c.print(f'Serving {name} on port {port} (Mode : {mode})', color='yellow')
+        server = c.module(f'server.{mode}')(ip=ip, port=port,module = self,name= name,
                              whitelist=whitelist,blacklist=blacklist)
         
+
+        # ensure that the module has a name
+        for k in ['module_name', 'module_id', 'name']:
+            if k not in self.__dict__:
+                self.__dict__[k] = name
         # register the server
         self.server_info = server.info
         self.ip = server.ip
@@ -2544,13 +2530,8 @@ class c:
             self.config = cls.munch({})
         self.config['info'] = self.info()
         
-
-        # self.set_key(key)
-            
-        # serve the server
-        server.serve(wait_for_termination=wait_for_termination,register=True)
         if wait_for_server:
-            cls.wait_for_server(name=module_name, timeout=wait_for_server_timeout, sleep_interval=wait_for_server_sleep_interval)
+            cls.wait_for_server(name=module_name)
 
     serve_module = serve
     @classmethod
