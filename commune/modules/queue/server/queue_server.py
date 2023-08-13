3b6581c37179b91e2ab1f1d80de0c47447fcfe4d
Your Name <you@example.com>
2023-08-13 13:34:27 -0400

    bit

--- commune/modules/queue/server/queue_server.py
+++ commune/modules/queue/server/queue_server.py
@@ -3,39 +3,45 @@ import asyncio
 import commune as c
 
 class QueueServer(c.Module):
-    def __init__(self, max_size=1000, default_key = 'default', **kwargs):
-        self.loop = c.get_event_loop()
+    def __init__(self, max_size=1000, mode = 'asyncio', **kwargs):
+        self.q = {}
         self.max_size = max_size
-        self.queue_map = {}
-        self.add_queue(default_key)
-
+        self.mode = mode
 
     def queue_exists(self, key:str):
-        return bool(key in self.queue_map)
+        return bool(key in self.q)
         
-    def add_queue(self, key:str, refresh=False, max_size=None, **kwargs):
+    def add_queue(self, key:str, 
+                  refresh:bool=False, 
+                  max_size:int=None, 
+                  mode:str=None, **kwargs):
+        if mode == None:
+            mode = self.mode
+        if max_size == None:
+            max_size = self.max_size
+        
+
         max_size = self.max_size if max_size is None else max_size
-        self.queue_map[key] = asyncio.Queue(max_size,**kwargs)
+
+        if mode == 'asyncio':
+            self.q[key] = asyncio.Queue(max_size,**kwargs)
+        else:
+            raise NotImplemented
+            
         return self.queue_map[key]
-
-    def get_queue(self, key, max_queue_size=1000, *args, **kwargs):
-        if key not in self.queue_map:
-            self.add_queue(key, max_size=max_queue_size, *args, **kwargs)
-        return self.queue_map[key]
-
-
-    async def async_get(self, key, *args, **kwargs):
-        q = self.get_queue(key)
-        return q.get(*args, **kwargs)
+    
     def get(self, *args, **kwargs):
-        return self.loop.run_until_complete(self.async_get(*args, **kwargs))
-
+        return c.gather(self.async_get(*args, **kwargs))
+    async def async_get(self, key, **kwargs):
+        q = self.get_queue(key)
+        return q.get(key, **kwargs)
+    
     async def async_put(self, key, value, sync=True, *args, **kwargs):
         q = self.get_queue(key,*args, **kwargs)
         job = q.put(value)
         return job
     def put(self, *args, **kwargs):
-        return self.loop.run_until_complete(self.async_put(*args, **kwargs))
+        return c.gather(self.async_put(*args, **kwargs))
 
     def async_run(self, job):
         return self.loop.run_until_complete(job)
--- commune/modules/storage/storage.py
+++ commune/modules/storage/storage.py
@@ -1,43 +1,37 @@
-import commune
+import commune as c
 from typing import *
 import streamlit as st
 
-class Storage(commune.Module):
+class Storage(c.Module):
 
     def __init__(self, store: Dict = None, key: 'Key' = None):
-        self.set_storage(store)
+        self.store = {}
         self.set_key(key)
     
-    def set_storage(self, storage: Dict = None):
-        storage = {} if storage == None else storage
-        assert isinstance(storage, dict), 'storage must be a dictionary'
-        self.storage = storage
+    def set_store(self, store: Dict = None):
+        store = {} if store == None else store
+        assert isinstance(store, dict), 'store must be a dictionary'
+        self.store = store
             
-    def put(self,
-            k:str, 
-            v: Any,
-            key: str = None,
-            encrypt: bool = False,
-            lifetime: bool = None) -> str:
-        kwargs = c.locals2kwargs(locals())
-        k =  kwargs.pop('k') # rm k
-        self.storage[k] = kwargs
-        return True
+    def put(self,k:str, v: Any) -> str:
+        self.store[k] = v
+        return {'success': True, 'msg': 'set to k'}
     
+
     def state_dict(self):
         import json
         state_dict = {}
-        for k, v in self.storage.items():
+        for k, v in self.store.items():
             try:
                 state_dict[k] = json.dumps(v)
             except:
-                commune.log(f'could not serialize {k}')
+                c.log(f'could not serialize {k}')
             
         return state_dict
     def from_state_dict(self, state_dict: Dict) -> None:
         import json
         for k, v in state_dict.items():
-            self.storage[k] = json.loads(v)
+            self.store[k] = json.loads(v)
             
     def save(self, tag: str = None):
         tag = self.resolve_tag(tag)
@@ -54,7 +48,7 @@ class Storage(commune.Module):
             key:str = None,
             max_staleness: int = 1000) -> Any:
         key = c.resolve_key(key)
-        item = self.storage[k]
+        item = self.store[k]
         verified = key.verify(item)
         # decrypt if necessary
         if self.is_encrypted(item):
@@ -62,7 +56,7 @@ class Storage(commune.Module):
         item['data'] = self.str2python(item['data'])
         assert verified, 'could not verify signature'
         # check staleness
-        staleness = commune.time() - item['data']['time']
+        staleness = c.time() - item['data']['time']
         assert staleness < max_staleness
 
         return item['data']['data']
@@ -70,7 +64,7 @@ class Storage(commune.Module):
     @property
     def key2address(self) -> Dict:
         key2address = {}
-        for k, v in self.storage.items():
+        for k, v in self.store.items():
             id = v['ss58_address']
             if id  in key2address:
                 key2address[v['ss58_address']] += [k]
