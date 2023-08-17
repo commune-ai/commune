b22cf7ca28941f29ad7fc81f80ed27ad872652b8
zong <you@example.com>
2023-08-17 04:21:39 +0000

    threads and queues

--- commune/modules/queue/queue.py
+++ commune/modules/queue/queue.py
@@ -4,12 +4,12 @@ import commune as c
 
 class QueueServer(c.Module):
     def __init__(self, max_size=1000, mode = 'asyncio', **kwargs):
-        self.q = {}
+        self.queues = {}
         self.max_size = max_size
         self.mode = mode
 
     def queue_exists(self, key:str):
-        return bool(key in self.q)
+        return bool(key in self.queues)
         
     def add_queue(self, key:str, 
                   refresh:bool=False, 
@@ -20,36 +20,31 @@ class QueueServer(c.Module):
         if max_size == None:
             max_size = self.max_size
         
-
         max_size = self.max_size if max_size is None else max_size
 
         if mode == 'asyncio':
-            self.q[key] = asyncio.Queue(max_size,**kwargs)
+            self.queues[key] = asyncio.Queue(max_size,**kwargs)
         else:
             raise NotImplemented
             
-        return self.queue_map[key]
-    
-    def get(self, *args, **kwargs):
-        return c.gather(self.async_get(*args, **kwargs))
-    async def async_get(self, key:str, **kwargs):
-        q = self.get_queue(key)
-        return q.get(key, **kwargs)
-    
-    async def async_put(self, key, value, sync=True, *args, **kwargs):
-        q = self.get_queue(key,*args, **kwargs)
-        job = q.put(value)
-        return job
-    def put(self, *args, **kwargs):
-        return c.gather(self.async_put(*args, **kwargs))
+        return self.queues[key]
 
-    def async_run(self, job):
-        return self.loop.run_until_complete(job)
 
+    def get(self, key:str, sync=False, **kwargs):
+        q = self.queues[key]
+        job = q.get(**kwargs)
+        return c.gather(job)
+    
+    def put(self, key, value, sync=False, **kwargs):
+        q = self.queues[key]
+        job = q.put(value, **kwargs)
+        if future:
+            return job
+        return  c.gather(job)
 
 
     def get_batch(self, key, batch_size=10, sync=False, **kwargs):
-        q = self.get_queue(key)
+        q = self.queues[key]
         batch_size = min(batch_size, q.qsize())
         jobs = [self.get(key, sync=False) for i in range(batch_size)]
         job = asyncio.gather(*jobs)
@@ -59,15 +54,31 @@ class QueueServer(c.Module):
 
     def size(self, key):
         # The size of the queue
-        return self.queue_map[key].qsize()
+        return self.queues[key].qsize()
 
     def empty(self, key):
         # Whether the queue is empty.
-        return self.queue_map[key].empty()
+        return self.queues[key].empty()
 
     def full(self, key):
         # Whether the queue is full.
-        return self.queue_map[key].full()
+        return self.queues[key].full()
 
     def size_map(self):
-        return {k: self.size(k) for k in self.queue_map}
+        return {k: self.size(k) for k in self.queues}
+
+
+    def test(self):
+        for i in range(100):
+            self.add_queue(i)
+            for j in range(100):
+                self.put(i, j)
+                assert self.size(i) == j + 1, f'{self.size(i)} != {j + 1}'
+
+            size = self.size(i)
+            for j in range(100):
+                self.get(i)
+                assert self.size(i) == size - j -1, f'{self.size(i)} != {size - j -1}'
+
+        return  {'success': True, 'message': 'QueueServer test passed'}
+            
--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,48 @@
-import commune as c
-model = c.connect('model.vicuna.7b', network='local')
-import json
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    pipeline,
+    BitsAndBytesConfig,
+)
+import torch
 
 
-def ask(question='What is the meaning of Life? options: [0,2,3,4]', output_schema={'answer': 'str'}):
-    output_schema = json.dumps(output_schema)
-    prompt = f"{question} \nANSWER the MC json({output_schema}):\n```json"
-    output = model.generate(prompt, max_new_tokens=15)
-    return output
-c.print(ask())
+name = "meta-llama/Llama-2-70b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(name)
+tokenizer.pad_token_id = tokenizer.eos_token_id    # for open-ended generation
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    name,
+    quantization_config=bnb_config,
+    device_map="auto",
+    trust_remote_code=True,
+)
+generation_pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    trust_remote_code=True,
+    device_map="auto",    # finds GPU
+)
+
+text = "any text "    # prompt goes here
+
+sequences = generation_pipe(
+    text,
+    max_length=128,
+    pad_token_id=tokenizer.pad_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    do_sample=True,
+    top_k=10,
+    temperature=0.4,
+    top_p=0.9
+)
+
+print(sequences[0]["generated_text"])
