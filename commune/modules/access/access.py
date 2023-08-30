54e4e280a07a08674d28681999e704d45540047a
shane <you@example.com>
2023-08-30 21:33:52 +0000

    subspace access

--- commune/modules/access/access.py
+++ commune/modules/access/access.py
@@ -3,7 +3,9 @@ import commune as c
 class Access(c.Module):
     def __init__(self, **kwargs):
         config = self.set_config(config=kwargs)
-    def verify(self):
+    def verify(self, **kwargs):
+        
+        kwargs['address']
         print('Base run')
 
 
