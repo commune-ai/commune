b26179b201272bf7727a81a45dcd759af7a9ce84
shane <you@example.com>
2023-09-06 21:38:01 +0000

    subspace additions

--- commune/modules/subspace/subspace.py
+++ commune/modules/subspace/subspace.py
@@ -783,7 +783,7 @@ class Subspace(c.Module):
             wait_for_inclusion: bool = False,
             wait_for_finalization: bool = True,
             network:str = None,
-            existential_deposit: float = 0.1,
+            existential_deposit: float = 0.5,
             sync: bool = False
         ) -> bool:
         network = self.resolve_network(network)
