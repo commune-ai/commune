32eba87db2427043aef80d8fd4d982035dc96fb6
Your Name <you@example.com>
2023-09-16 23:38:25 +0000

    hey

--- commune/modules/storage/vali.py
+++ commune/modules/storage/vali.py
@@ -13,9 +13,59 @@ class StorageVali(c.Module):
 
 
 
-    def score_module(module) -> float:
-        remote_has = self.storage.score_module(module)
+    storage_peers = {}
 
+
+    def get_storage_peer(self, name, default=None) -> Dict:
+        if default == None:
+            default = {'address': None,  'size': 0, 'stored_keys': [], 'w': 0}
+        return self.get(f'{self.tag}/peers/{name}', {})
+
+
+    def score_module(self, module, **kwargs) -> float:
+
+
+        info = module.info()
+        obj_keys = self.storage.ls_keys()
+        key = c.choice(obj_keys)
+        
+        obj = self.storage.get(key)
+
+        obj_size = c.sizeof(obj)
+
+        remote_has = self.remote_has(remote_obj_key, module=module)
+        if not remote_has:
+            module.put(key, obj)
+        
+
+        storage_peer = self.get_storage_peer(info['name'])
+
+        remote_has = self.remote_has(remote_obj_key, module=module)
+        if remote_has:
+            if key not in storage_peer['stored_keys']:
+                storage_peer['stored_keys'] += [key]
+                storage_peer['size'] += obj_size
+        
+        if not remote_has:
+            remote_obj_key = obj['hash']
+            module.put(remote_obj_key, obj)
+            remote_has = self.remote_has(remote_obj_key, module=module)
+        if remote_has:
+            if key not in storage_peer['stored_keys']:
+                storage_peer['stored_keys'] += [key]
+                storage_peer['size'] += obj_size
+        else:
+
+            storage_peer['size'] -= obj_size
+            storage_peer['size'] = max(storage_peer['size'], 0)
+            storage_peer['stored_keys'] = [k for k in storage_peer['stored_keys'] if k != key]
+
+        # set weight
+        storage_peer['w'] = storage_peer['size']
+        
+
+        return storage_peer
+        
 
 
     def put(self, *args,**kwargs):
