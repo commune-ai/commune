e7feb29721b11648afffb26cebb121d6c33dad01
Your Name <you@example.com>
2023-09-21 13:52:57 -0400

    tools

---
+++ commune/modules/tool/belt.py
@@ -0,0 +1,10 @@
+import commune as c
+
+class ToolBelt(c.Module):
+    def __init__(
+        self,
+        tools = [],
+    ):
+        self.tool_map = {tool.name:tool for tool in tools}
+    def forward(self,tool:str , *args, **kwargs, ):
+        self.tool_map[tool].forward(*args, **kwargs)
