3f7ae554010118f1fcf76d01fce2ad7ce6e5fa31
Your Name <you@example.com>
2023-09-25 14:56:17 +0000

    made changes to server monitoring and some reduction in the main module.py file

--- commune/modules/litellm/litellm.py
+++ commune/modules/litellm/litellm.py
@@ -1,10 +1,14 @@
 import commune as c
 
-class Litellm(c.Module):
-    def __init__(self, **kwargs):
-        config = self.set_config(kwargs)
-    def run(self, x: int = 1, y: int = 2):
-        a = x + y
-        return {'status': 'success', 'answer': a, 'x': x, 'y': y}
 
+class LiteLLM(c.Module):
 
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.name = "LiteLLM"
+        self.description = "A lightweight LLM module"
+        self.version = "0.0.1"
+
+    async def on_message(self, message):
+        if message.content.startswith("!llm"):
+            await message.channel.send("LiteLLM is working!")
