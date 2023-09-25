ba145a37bc36b464d64e5508e41634bd348e6efa
Your Name <you@example.com>
2023-09-25 22:51:40 +0000

    refactor

--- commune/modules/petals/src/petals/client/__init__.py
+++
@@ -1,4 +0,0 @@
-from petals.client.config import ClientConfig
-from petals.client.inference_session import InferenceSession
-from petals.client.remote_sequential import RemoteSequential
-from petals.client.routing import NoSpendingPolicy, RemoteSequenceManager, SpendingPolicyBase
