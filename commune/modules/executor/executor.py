ca75c9d535b41470e5414346d5fa35f683451f41
Your Name <you@example.com>
2023-10-10 06:20:19 +0000

    update

--- commune/modules/executor/executor.py
+++ commune/modules/executor/executor.py
@@ -86,7 +86,6 @@ class TaskExecutor(c.Module):
                 del kwargs["priority"]
             task = Task(fn=fn, args=args, kwargs=kwargs, timeout=timeout)
             # add the work item to the queue
-            c.print(f"Adding task with priority {priority}", color='green')
             self.work_queue.put((priority, task), block=False)
             # adjust the thread count to match the new task
             self.adjust_thread_count()
