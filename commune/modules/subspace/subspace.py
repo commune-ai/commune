8c4c16cdb2a7ab731d6bb8dee6d13bf493f294ec
Your Name <you@example.com>
2023-10-12 19:59:53 +0000

    update

--- commune/modules/subspace/subspace.py
+++ commune/modules/subspace/subspace.py
@@ -3364,7 +3364,7 @@ class Subspace(c.Module):
                  port:int=None,
                  rpc_port:int=None,
                  ws_port:int=None,
-                 telemetry_url:str = None,
+                 telemetry_url:str = False,
                  purge_chain:bool = True,
                  refresh:bool = False,
                  verbose:bool = False,
@@ -3375,6 +3375,7 @@ class Subspace(c.Module):
                  validator:bool = False,
                  local:bool = False,
                  ip = None,
+                 sync:bool = 'fast', 
                  max_boot_nodes:int = 24,
                  
                  ):
@@ -3406,16 +3407,17 @@ class Subspace(c.Module):
         if purge_chain:
             cls.purge_chain(base_path=base_path)
             
+
         cmd_kwargs = f' --base-path {base_path}'
 
         chain_spec_path = cls.chain_spec_path(chain)
         cmd_kwargs += f' --chain {chain_spec_path}'
 
-        if telemetry_url == None:
-            telemetry_url = cls.telemetry_url(chain=chain)
-        cmd_kwargs += f' --telemetry-url {telemetry_url}'
-
-
+        if telemetry_url != None:
+            if telemetry_url == True:
+                telemetry_url = cls.telemetry_urls(chain=chain)['shard']
+            cmd_kwargs += f" --telemetry-url '{telemetry_url}'"
+        cmd_kwargs += f" --sync {sync}"
         
         if validator :
             cmd_kwargs += ' --validator'
@@ -3473,7 +3475,6 @@ class Subspace(c.Module):
             logs_sig = ' is already in use by container "'
             if logs_sig in output:
                 container_id = output.split(logs_sig)[-1].split('"')[0]
-                c.print(container_id)
                 c.module('docker').rm(container_id)
                 output = c.cmd(cmd, verbose=True)
         else: 
@@ -3720,47 +3721,77 @@ class Subspace(c.Module):
         c.print(stats)
         assert isinstance(stats, list) 
 
-    telemetry_image = 'parity/substrate-telemetry-backend'
+    telemetry_backend_image = 'parity/substrate-telemetry-backend'
+    telemetry_frontend_image = 'parity/substrate-telemetry-frontend'
     @classmethod
     def install_telemetry(cls):
         c.cmd(f'docker build -t {cls.telemetry_image} .', sudo=False, bash=True)
 
+
+
     @classmethod
-    def start_telemetry(cls, port:int=None, network:str='host',  name='telemetry',  chain=chain):
-        port_core, port_shard = c.free_ports(n=2)
-        c.print(f'port_core: {port_core}, port_shard: {port_shard}')
-        cmd = {}
-        cmd['core'] = f"docker run   --network={network} --name {name} \
-                --read-only \
-                {cls.telemetry_image} \
-                telemetry_core -l 0.0.0.0:{port_core}"
+    def start_telemetry(cls, 
+                    port:int=None, 
+                    network:str='host', 
+                    name='telemetry', 
+                    chain=chain, 
+                    trials:int=3, 
+                    reuse_ports:bool=True,
+                    frontend:bool = True):
 
-        shard_name = f'{name}.shard'
-        cmd['shard'] = f"docker run  --network={network} \
-                    --name {shard_name} \
-                    --read-only \
-                     {cls.telemetry_image} \
-                    telemetry_shard -l 0.0.0.0:{port_shard} -c http://0.0.0.0:{port_core}/shard_submit"
-
-        config = cls.config()
-
+        names = {'core': name, 'shard': f'{name}.shard', 'frontend': f'{name}.frontend'}
         docker = c.module('docker')
-        if docker.exists(name):
-            docker.kill(name)
-        if docker.exists(shard_name):
-            docker.kill(shard_name)
+        config = cls.config()
+        success = False
+        cmd = {}
         output = {}
-        output['core'] = c.cmd(cmd['core'])
-        output['shard'] = c.cmd(cmd['shard'])
+        k = f'chain_info.{chain}.telemetry_urls'
+        telemetry_urls = cls.get(k, {})
 
-        success = bool('error' not in output['core'].lower()) and bool('error' not in output['shard'].lower())
+        while trials > 0 and success == False:
+            ports = {}
+            ports['core'], ports['shard'], ports['frontend'] = c.free_ports(n=3, random_selection=True)
+            if reuse_ports:
+                telemetry_urls = cls.getc(k, {})
+            if len(telemetry_urls) == 0:
+                telemetry_urls[name] = {'shard': f"ws://{c.ip()}:{ports['shard']}/submit 0", 
+                                        'feed': f"ws://{c.ip()}:{ports['core']}/feed", 
+                                        'frontend': f'http://{c.ip()}:{ports["frontend"]}'}
+                reuse_ports = False
+
+            if reuse_ports:
+                ports = {k:int(v.split(':')[-1].split('/')[0]) for k, v in telemetry_urls.items()}
+            cmd['core'] = f"docker run  -d --network={network} --name {names['core']} \
+                        --read-only \
+                        {cls.telemetry_backend_image} \
+                        telemetry_core -l 0.0.0.0:{ports['core']}"
+
+            cmd['shard'] = f"docker run  -d --network={network} \
+                        --name {names['shard']} \
+                        --read-only \
+                        {cls.telemetry_backend_image} \
+                        telemetry_shard -l 0.0.0.0:{ports['shard']} -c http://0.0.0.0:{ports['core']}/shard_submit"
+
+            cmd['frontend'] = f"docker run -d  \
+                    --name {names['frontend']} \
+                    -p {ports['frontend']}:8000\
+                    -e SUBSTRATE_TELEMETRY_URL={telemetry_urls[name]['feed']} \
+                    {cls.telemetry_frontend_image}"
+
+            for k in cmd.keys():
+                if docker.exists(names[k]):
+                    docker.kill(names[k])
+                output[k] = c.cmd(cmd[k])
+                logs_sig = ' is already in use by container "'
+                if logs_sig in output[k]:
+                    container_id = output[k].split(logs_sig)[-1].split('"')[0]
+                    docker.rm(container_id)
+                    output[k] = c.cmd(cmd[k], verbose=True)
         
-        if success:
-            k = f'chain_info.{chain}.telemetry_urls'
-            telemtry_urls = cls.get(k, {})
-            telemtry_urls[name] = c.ip() + ':' + str(port)
-            cls.putc(k, telemtry_urls)
-
+            success = bool('error' not in output['core'].lower()) and bool('error' not in output['shard'].lower())
+            trials -= 1
+            if success: 
+                cls.putc(k, telemetry_urls)
         return {
             'success': success,
             'cmd': cmd,
@@ -3768,14 +3799,24 @@ class Subspace(c.Module):
         }
 
     @classmethod
-    def telemetry_urls(cls, chain=chain):
-        return list(cls.getc(f'chain_info.{chain}.telemetry_urls', {}).values())
+    def telemetry_urls(cls, name = 'telemetry', chain=chain):
+        telemetry_urls = cls.getc(f'chain_info.{chain}.telemetry_urls', {})
+        assert len(telemetry_urls) > 0, f'No telemetry urls found for {chain}, c start_telemetry'
+        return telemetry_urls[name] 
+
+
     @classmethod
-    def telemetry_url(cls, chain=chain):
-        url =  c.choice(cls.telemetry_urls(chain=chain))
+    def telemetry_url(cls,endpoint:str='submit', chain=chain, ):
+
+
+        telemetry_urls = cls.telemetry_urls(chain=chain)
+        if telemetry_urls == None:
+            raise Exception(f'No telemetry urls found for {chain}')
+        url = telemetry_urls[endpoint]
+
         if not url.startswith('ws://'):
             url = 'ws://' + url
-        url = url.replace(c.ip(), '0.0.0.0') + '/feed'
+        url = url.replace(c.ip(), '0.0.0.0')
         return url
 
     @classmethod
