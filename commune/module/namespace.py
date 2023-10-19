f2a171a544b72daa4a11f8d37f6cc60033d58ed1
Your Name <you@example.com>
2023-10-19 16:56:04 -0400

    update

--- commune/module/namespace.py
+++ commune/module/namespace.py
@@ -146,3 +146,36 @@ class Namespace(c.Module):
         namespace = c.get_json('local_namespace', {})
         c.put_namespace(network, namespace)
 
+    remote_modules_path ='remote_modules'
+    @classmethod
+    def add_server(cls, address:str, name=None, network:str = 'local', **kwargs):
+        module = c.connect(address)
+        module_info = module.info()
+        name = module_info['name'] if name == None else name
+        c.register_server(name, address, network=network)
+
+        path = cls.remote_modules_path
+        # register the server info
+        remote_modules = c.get(path, {})
+        remote_modules[name] = module_info
+        c.put(path, remote_modules )
+        return {'success': True, 'msg': f'Added {address} to remote modules', 'remote_modules': remote_modules}
+    
+    @classmethod
+    def remote_servers(cls, network:str = 'local', **kwargs):
+        return c.servers(network=network)
+
+    @classmethod
+    def rm_server(cls,  name, network:str = 'local', **kwargs):
+        if c.server_exists(name, network=network):
+            # reregister
+            address = c.get_address(name, network=network)
+            c.deregister_server(name, network=network)
+            remote_modules = c.get(cls.remote_modules_path, {})
+            remote_modules.pop(name, None)
+            servers = c.servers(network=network)
+            assert c.server_exists(name, network=network) == False, f'{name} still exists'
+            return {'success': True, 'msg': f'removed {address} to remote modules', 'servers': servers, 'network': network}
+        else:
+            return {'success': False, 'msg': f'{name} does not exist'}
+        
