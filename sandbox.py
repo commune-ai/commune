1a1fb9fd6275697266784ae0ed205ddce114546a
Your Name <you@example.com>
2023-10-23 14:25:08 -0400

    update

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,13 @@
 import commune as c
 
-x = 'fam wahtdup'
+# x = 'fam wahtdup'
+# servers = c.servers(network='remote')
+# c.print(servers)
+# c.print(c.submit('module', fn = 'submit',  kwargs=dict(fn='print', args=[x], network='remote')))
 
-c.print(c.submit('module', fn = 'submit',  kwargs=dict(fn='print', args=[x], network='remote',)))
+
+output = c.rcmd('c addy')
+network = 'remote'
+c.rm_namespace(network=network)
+for v in output.values():
+    c.add_server(v[0], network=network)
