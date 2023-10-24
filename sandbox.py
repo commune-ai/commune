0fe892dbd41b0bdd505d01f5bb186eb6176e0faf
Your Name <you@example.com>
2023-10-24 19:25:13 +0000

    update

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,5 @@
 import commune as c
 
-addresses = c.addresses('module', network='remote')
-c.print(c.call(addresses[0], fn='submit', kwargs={'fn': 'subspace.start_node', 'kwargs': {'node': 'alice'}}))
+# addresses = c.addresses('module', network='remote')
+# c.print(c.call(addresses[0], fn='submit', kwargs={'fn': 'subspace.start_node', 'kwargs': {'node': 'alice'}}))
+c.rcmd('c pull', verbose=True)
