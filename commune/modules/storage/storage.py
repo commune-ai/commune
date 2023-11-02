a7cb123219c866a06bc8935ce40b46154f17bc9f
Your Name <you@example.com>
2023-11-02 14:46:00 -0400

    update

--- commune/modules/storage/storage.py
+++ commune/modules/storage/storage.py
@@ -3,15 +3,15 @@ from typing import *
 import streamlit as st
 
 class Storage(c.Module):
-    whitelist: List = ['put', 'get', 'item_hash']
+    whitelist: List = ['put_item', 'get_item', 'item_hash']
 
     def __init__(self, 
-                 max_replicas:int = 1, 
+                 max_replicas:int = 2, 
                 network='local',
                 validate:bool = False,
                 match_replica_prefix : bool = False,
+                tag = None,
                 **kwargs):
-        self.replica_map = {}
         self.max_replicas = max_replicas
         self.network = network
         self.set_config(kwargs=locals()) 
@@ -19,25 +19,31 @@ class Storage(c.Module):
         self.executor = c.module('executor')()
         if validate:
             self.match_replica_prefix = match_replica_prefix
-            c.thread(self.validate)
+            c.thread(self.validate_loop)
 
-    @property
-    def store_dirpath(self) -> str:
-        tag = self.tag
+
+    def resolve_tag(self, tag=None):
+        tag = tag if tag != None else self.tag
+        return tag
+    
+
+    def store_dirpath(self, tag=None) -> str:
+        tag = self.resolve_tag(tag)
         if tag == None:
             tag = 'base'
         return self.resolve_path(f'store/{tag}')
 
-    def resolve_store_path(self, key: str) -> str:
-        path =  f'{self.store_dirpath}/{key}'
+    def resolve_item_path(self, key: str, tag=None) -> str:
+        path =  f'{self.store_dirpath(tag=tag)}/{key}'
         return path
     
 
     def num_files(self) -> int:
         return len(self.files)
     
-    def files(self) -> List:
-        return c.ls(self.store_dirpath)
+    def files(self, tag=None) -> List:
+        files = c.ls(self.store_dirpath(tag=tag))
+        return sorted(files)
     
     def item2info(self, search=None):
         files = self.files()
@@ -54,10 +60,7 @@ class Storage(c.Module):
             item2info[name] = file_info
         
         return item2info
-    @property
-    def items(self, search=None) -> List:
-        items =  list(self.item2info(search=search).keys())
-        return items
+
     def file2size(self, fmt:str='b') -> int:
         files = self.files()
         file2size = {}
@@ -72,24 +75,41 @@ class Storage(c.Module):
             key = c.get_key(key)
         return key
 
-    def put_item(self, k,  v: Dict, encrypt:bool=False, replicas = 1, key=None):
+    def put_item(self, k,  v: Dict, encrypt:bool=False, replicas = 1, tag=None):
         timestamp = c.timestamp()
-        obj = v
-
-        k = self.resolve_store_path(k)
+        k = self.resolve_item_path(k, tag=tag)    
+        path = {
+            'data': k +'/data',
+            'metadata': k + '/metadata'
+        }    
         # serialize
-        v = self.serializer.serialize(obj)
+        data = self.serializer.serialize(v)
+
+        # encrypt it if you want
         if encrypt:
-            v = self.key.encrypt(v)
-        v = self.key.sign(v, return_json=True)
-        v['encrypted'] = encrypt
-        v['timestamp'] = timestamp
-        
-        if replicas > 1:
-            self.replicate(k, v, replicas=replicas)
-        self.put_json(k, v)
+            data = self.key.encrypt(data)
+            
+        # sign it for verif
+        data = self.key.sign(data, return_json=True)
+
+        c.makedirs(k, exist_ok=True)
+        self.put_json(path['data'], data)
+
+
+        # SAVE METADATA 
+        metadata = {
+            'size_bytes': self.sizeof(data),
+            'timestamp': timestamp,
+            'encrypt': encrypt,
+            'key': self.key.ss58_address ,
+            'size_bytes': c.format_data_size(c.filesize(path['data']), fmt='b'),
+            'path': path
+        }
+
+        self.put_json(path['metadata'], metadata)
+
         size_bytes = self.sizeof(v)
-        return {'success': True, 'key': k,  'size_bytes': size_bytes, 'replica_map': self.replica_map}
+        return {'success': True, 'key': k,  'metadata': metadata}
     
     def replicate(self, k, v, replicas=2):
         replica_map = self.get('replica_map', default={})
@@ -97,25 +117,31 @@ class Storage(c.Module):
         peer.put_item(k, v)
         replica_map[k] = [peer]
 
+    def rm_item(self, k):
+        k = self.resolve_item_path(k)
+        return c.rm(k)
+    
+    def rm_items(self, search):
+        items = self.items(search=search)
+        for item in items:
+            self.rm_item(item)
+        return {'success': True, 'items': items}
+
     def get_item(self,k, deserialize:bool= True, key=None) -> Any:
-        k = self.resolve_store_path(k)
-        v = self.get_json(k, {})
-
-        if 'data' not in v:
+        k = self.resolve_item_path(k)
+        data = self.get_json(k+'/data', {})
+        metadata = self.get_json(k+'/metadata', {})
+        if 'data' not in data:
             return {'success': False, 'error': 'No data found'}
-        c.print(v)
-        if 'encrypted' in v and v['encrypted']:
-            c.print(v)
-            v['data'] = self.key.decrypt(v['data'])
-
+        if 'encrypted' in metadata and metadata['encrypted']:
+            data['data'] = self.key.decrypt(data['data'])
 
         if deserialize:
-            v['data'] = self.serializer.deserialize(v['data'])
-        return v['data']
+            data['data'] = self.serializer.deserialize(data['data'])
+        return data['data']
     
-    
-    def replicate(self, k, module) -> str:
-        self.replicas
+    def get_metadata(self, k, tag=None):
+        return c.get_json(self.resolve_item_path(k, tag=tag)+'/metadata')  
 
     def item_hash(self, k: str = None, seed : int= None , seed_sep:str = '<SEED>', obj=None) -> str:
         if obj == None:
@@ -129,59 +155,121 @@ class Storage(c.Module):
         return c.timestamp() if seed == None else seed
 
         
-    def exists(self, k) -> bool:
-        path = self.resolve_store_path(k)
+    def exists(self, k, tag=None) -> bool:
+        path = self.resolve_item_path(k, tag=tag)
         return c.exists(path)
     has = exists
 
-    def rm(self, k) -> bool:
-        assert self.exists(k), f'Key {k} does not exist'
-        path = self.resolve_store_path(k)
+    def rm(self, k , tag=None) -> bool:
+        assert self.exists(k, tag=tag), f'Key {k} does not exist with {tag}'
+        path = self.resolve_item_path(k, tag=tag)
         return c.rm(path)
+    
 
-    def items(self, search=None) -> List:
-        path = self.store_dirpath
-        path += f'/{search}' if search != None else ''
-        return c.ls(path)
+    def item_paths(self, tag=None):
+        path = self.store_dirpath(tag=tag)
+        return [x for x in c.ls(path)]
 
-    def refresh(self) -> None:
-        path = self.store_dirpath
-        return c.rm(path)
-
-    @property
-    def key2address(self) -> Dict:
-        key2address = {}
-        for k, v in self.store.items():
-            id = v['ss58_address']
-            if id  in key2address:
-                key2address[v['ss58_address']] += [k]
-            else:
-                key2address[v['ss58_address']] = [k]
-        return key2address
-
-    @classmethod
-    def cachefn(cls, func, max_age=60, update=False, cache=True, cache_folder='cachefn'):
-        import functools
-        path_name = cache_folder+'/'+func.__name__
-        def wrapper(*args, **kwargs):
-            fn_name = func.__name__
-            cache_params = {'max_age': max_age, 'cache': cache}
-            for k, v in cache_params.items():
-                cache_params[k] = kwargs.pop(k, v)
-            if not update:
-                result = cls.get(fn_name, default=None, **cache_params)
-                if result != None:
-                    return result
-
-            result = func(*args, **kwargs)
-            
-            if cache:
-                cls.put(fn_name, result, cache=cache)
-
-            return result
-
-        return wrapper
+    def items(self, search=None, include_replicas=False, tag=None) -> List:
+        path = self.store_dirpath(tag=tag)
+        items = [x.split('/')[-1] for x in c.ls(path)]
         
+        if search != None:
+            items = [x for x in items if search in x]
+
+        if include_replicas == False:
+            items = [x for x in items if "replica::" not in x]
+
+        return items
+    
+    def replicas(self, tag=None) -> List:
+        return self.items(search='replica::', tag=tag)
+        
+
+    def refresh(self, tag=None) -> None:
+        path = self.store_dirpath(tag=tag)
+        return c.rm(path)
+
+
+    def replica_map(self):
+        return self.get(f'replica_map/{self.tag}', default={})
+    
+    def set_replica_map(self, value):
+        self.put(f'replica_map/{self.tag}', value)
+
+    item2replicas = {}
+    def validate(self, refresh=False):
+
+        self.ls()
+
+        # get the item2info
+        item2info = self.item2info()
+        item_keys = list(item2info.keys())
+        item_key = c.choice(item_keys)
+        item = self.get_item(item_key)
+        item_hash = c.hash(item)
+        remote_item_key = f'replica::{item_hash}'
+
+        # get the peers 
+        peers = self.peers()
+        max_replicas = min(self.max_replicas, len(peers)) 
+
+        # get the replica_map
+        replica_map = {} if refresh else self.get(f'replica_map/{self.tag}',{})
+        replica_peers = replica_map.get(item_key, [])
+        has_enough_replicas = bool(len(replica_peers) >= max_replicas)
+        
+
+        if has_enough_replicas: 
+            # check if replicas match
+            peer = c.choice(replica_peers)
+            seed = c.timestamp()
+            # get the local hash
+            local_hash = self.item_hash(obj=item, seed=seed)
+
+            # check if remote hash matches
+            remote_hash= c.call(peer, 'item_hash', remote_item_key, seed=seed)
+
+            if local_hash != remote_hash:
+                # remove replica
+                replica_peers.remove(peer)
+                c.print('local_hash', local_hash)
+                c.print('remote_hash', remote_hash)
+                c.print(f'Hashes do not match for {item_key} on {peer}', color='red')
+            else:
+                c.print(f'Hashes match for {item_key} on {peer}', color='green')
+                
+        else:
+            # find peer to add replica
+            candidate_peers = [peer for peer in peers if peer not in replica_peers]
+            peer = c.choice(candidate_peers)
+            # add replica
+            response = c.call(peer, 'put_item', remote_item_key, item)
+            c.print(response, '')
+
+            if 'error' in response:
+                c.print(f'Failed to add replica for {item_key} on {peer} ', color='red')
+            else:
+                replica_peers += [peer]
+                c.print(f'Added replica for {item_key} on {peer}', color='green')
+
+        replica_map[item_key] = replica_peers
+        self.put(f'replica_map/{self.tag}', replica_map)
+
+        return {'success': True, 'replica_map': replica_map}
+    
+
+    def validate_loop(self, tag=None, interval=0.1, vote_inteval=1):
+        import time
+        tag = self.tag if tag == None else tag
+        while True:
+            try:
+                self.validate()
+                time.sleep(interval)
+            except Exception as e:
+                c.print(e, color='red')
+
+
     @classmethod
     def test(cls):
         c.print('STARTING')
@@ -202,70 +290,3 @@ class Storage(c.Module):
                 assert obj_str == obj_str, f'Failed to put {obj} and get {get_obj}'
 
                 self.rm('test')
-            
-    @classmethod
-    def test_verify(cls):
-        import torch
-        storage_modules = [cls() for i in range(10)]
-        object_list = [0, {'fam': 1}, 'whadup', {'tensor': torch.rand(3,3)}, {'tensor': torch.rand(3,3), 'fam': 1}]
-        for i, x in enumerate(object_list):
-            for i, storage in enumerate(storage_modules):
-                storage.put_item('test', x)
-            seed = c.time()
-            for i, storage_i in enumerate(storage_modules):
-                for j, storage_j in enumerate(storage_modules):
-                    c.print(f'Verifying i={i} j={j}')
-                    assert storage_i.item_hash('test', seed=seed) == storage_j.item_hash('test', seed=seed)
-
-
-    item2replicas = {}
-    def validate(self, refresh=False):
-
-        self.ls()
-        item2info = self.item2info()
-        item_keys = list(item2info.keys())
-        item_key = c.choice(item_keys)
-        item = self.get_item(item_key)
-        item_hash = c.hash(item)
-        remote_item_key = f'replica::{item_hash}'
-
-        # get the peers 
-        peers = self.peers()
-        max_replicas = min(self.max_replicas, len(peers)) 
-        # get the replica_map
-        replica_map = {} if refresh else self.get(f'replica_map/{self.tag}',{})
-        replica_peers = self.item2replicas.get(item_key, [])
-        has_enough_replicas = bool(len(replica_peers) >= max_replicas)
-        if has_enough_replicas: 
-            # check if replicas match
-            peer = c.choice(replica_peers)
-            seed = c.timestamp()
-            # get the local hash
-            local_hash = self.item_hash(obj=item, seed=seed)
-
-            # check if remote hash matches
-            remote_hash= c.connect(peer).item_hash(remote_item_key, seed=seed)
-            if local_hash != remote_hash:
-                # remove replica
-                replica_peers.remove(peer)
-                c.print(f'Hashes do not match for {item_key} on {peer}', color='red')
-            else:
-                c.print(f'Hashes match for {item_key} on {peer}', color='green')
-                
-        else:
-            # find peer to add replica
-            candidate_peers = [peer for peer in peers if peer not in replica_peers]
-            peer = c.choice(candidate_peers)
-            # add replica
-            response = c.call(peer, 'put', remote_item_key, item)
-
-            if response['success']:
-                replica_peers += [peer]
-                c.print(f'Added replica for {item_key} on {peer}', color='green')
-            else: 
-                c.print(f'Failed to add replica for {item_key} on {peer}', color='red')
-
-        replica_map[item_key] = replica_peers
-        self.put(f'replica_map/{self.tag}', replica_map)
-
-        return {'success': True, 'replica_map': replica_map}
