44d77332a53db78158408f399862d7e908f0f624
c-spider <spdrdev@gmail.com>
2023-11-09 04:36:30 +0000

    new function: text2text,speech2speech

--- commune/modules/model/text2speech/model_text2speech.py
+++ commune/modules/model/text2speech/model_text2speech.py
@@ -14,8 +14,8 @@ class ModelText2speech(c.Module):
         c.print(self.config, 'This is the config, it is a Munch object')
         return x + y
 
-    def text2speech(self, text, src_format='t2st', target_lang='eng', src_lang="eng", output_file="output.wav"):
-        translated_text, wav, sr = self.translator.predict(text, src_format, target_lang, src_lang)
+    def text2speech(self, text, target_lang='eng', src_lang="eng", output_file="output.wav"):
+        translated_text, wav, sr = self.translator.predict(text, 't2st', target_lang, src_lang)
         
         torchaudio.save(
             output_file,
@@ -26,12 +26,23 @@ class ModelText2speech(c.Module):
         c.print("Audio file created: ", output_file);
         return translated_text
 
-    def speech2text(self, src_format='s2tt', target_lang='eng', src_lang="eng", inputFile="audio.wav"):
-        translated_text, _, _ = self.translator.predict(inputFile, src_format, target_lang, src_lang)
+    def speech2text(self, target_lang='eng', src_lang="eng", inputFile="audio.wav"):
+        translated_text, _, _ = self.translator.predict(inputFile, 's2tt', target_lang, src_lang)
         c.print("Result: ", translated_text)
         return translated_text
 
-    def text2text(self, text, src_format='t2tt', target_lang='eng', src_lang="eng"):
-        translated_text, _, _ = self.translator.predict(text, src_format, target_lang, src_lang)
+    def text2text(self, text, target_lang='eng', src_lang="eng"):
+        translated_text, _, _ = self.translator.predict(text, "t2tt", target_lang, src_lang)
         return translated_text
     
+    def speech2speech(self, target_lang='eng', src_lang="eng", output_file="output.wav", input_file="audio.wav"):
+        translated_text, wav, sr = self.translator.predict(input_file, "s2st", target_lang, src_lang)
+        
+        torchaudio.save(
+            output_file,
+            wav[0],
+            sr,
+        )
+
+        c.print("Audio file created: ", output_file);
+        return translated_text
