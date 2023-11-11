c16c9fa73dcec3b2fed8cd83375d9131291ec3f2
purgeCOM <ToSKY627@outlook.com>
2023-11-11 03:52:28 -0500

    add gradio

--- commune/modules/model/text2image/model_text2image.py
+++ commune/modules/model/text2image/model_text2image.py
@@ -1,6 +1,9 @@
 import commune as c
 from diffusers import DiffusionPipeline
+import gradio as gr
 import torch
+
+
 
 class ModelText2image(c.Module):
     """
@@ -8,24 +11,38 @@ class ModelText2image(c.Module):
     """
     def __init__(self, config = None, **kwargs):
         self.set_config(config, kwargs=kwargs)
-        self.pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+        self.pipeline = DiffusionPipeline.from_pretrained("Linaqruf/anything-v3.0", torch_dtype=torch.float16)
         self.pipeline.to("cuda")
+
 
     def call(self, x:int = 1, y:int = 2) -> int:
         c.print(self.config.sup)
         c.print(self.config, 'This is the config, it is a Munch object')
         return x + y
     
-    def call(self, prompt, save_path="newimage"):
+    
+    def generate(self, Prompt, SaveFileName="newimage"):
         """
         Creates image from a given prompt.
 
         Optional parameters:
 
-        prompt:     Prompt to generate image.
+        prompt:        Prompt to generate image.
         save_path:     Path to the folder to save the results.
 
         """
-        image=self.pipeline(prompt).images[0]
-        image.save(f"{save_path}.jpg")
-        c.print(f"New image created. {save_path}.jpg")
+        image=self.pipeline(Prompt).images[0]
+        image.save(f"{SaveFileName}.jpg")
+        c.print(f"New image created. {SaveFileName}.jpg")
+        return image
+
+
+    def gradio(self):
+        """
+        This function for launching graio.
+        """
+        interface = gr.Interface(fn=self.generate, 
+                                inputs=["text", "text"],
+                                outputs="image", 
+                                title='Text to Image')
+        interface.launch(share=True, quiet=True)
--- commune/modules/model/text2image/readme.md
+++ commune/modules/model/text2image/readme.md
@@ -1,14 +1,15 @@
 # Convert Text to Image Module - README
 
-This module is designed for converting user prompt to call. It offers a `call` function that users can convert the text with customized parameters.
-Here is the detailed explanation of the `call` function. Also I added docstring so that you can easily commit.
+This module is designed for converting user prompt to call. It offers a `generate` function that users can convert the text with customized parameters.
+Here is the detailed explanation of the `generate` function. Also I added docstring so that you can easily commit.
 
 ## Here's a detailed explanation of parameters for convert:
 
--`prompt`:        Prompt to generate image.
--`save_path`:     Path to the folder to save the results.
+-`Prompt`:           Prompt to generate image.
+-`SaveFileName`:     Filename to save the results.
 
 
 ### Usage Example:
 
-`c model.text2image call prompt="a cat sitting on the chair" save_path="cat"`
+`c model.text2image generate Prompt="a cat sitting on the chair" SaveFileName="cat"`
+`c model.text2image gradio`
