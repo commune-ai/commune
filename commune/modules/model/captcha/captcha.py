affc1a2a612a613f9faa5f289e57bb91aaf6e6f0
shahjab <shahjab323@gmail.com>
2023-11-15 16:55:38 +0100

    feat (captcha): support for turnstile proxyless requests

--- commune/modules/model/captcha/captcha.py
+++ commune/modules/model/captcha/captcha.py
@@ -2,7 +2,7 @@ import commune as c
 import asyncio
 
 from capmonstercloudclient import CapMonsterClient, ClientOptions
-from capmonstercloudclient.requests import RecaptchaV2ProxylessRequest, RecaptchaV2Request, FuncaptchaProxylessRequest, FuncaptchaRequest, GeetestProxylessRequest, GeetestRequest, ImageToTextRequest, HcaptchaProxylessRequest, HcaptchaRequest, RecaptchaV2EnterpriseRequest, RecaptchaV2EnterpriseProxylessRequest, RecaptchaV3ProxylessRequest
+from capmonstercloudclient.requests import RecaptchaV2ProxylessRequest, RecaptchaV2Request, FuncaptchaProxylessRequest, FuncaptchaRequest, GeetestProxylessRequest, GeetestRequest, ImageToTextRequest, HcaptchaProxylessRequest, HcaptchaRequest, RecaptchaV2EnterpriseRequest, RecaptchaV2EnterpriseProxylessRequest, RecaptchaV3ProxylessRequest, TurnstileProxylessRequest
 
 class Captcha(c.Module):
     def __init__(self, api_key:str = None, host='https://api.capmonster.cloud/', cache_key:bool = True):
@@ -254,7 +254,7 @@ class Captcha(c.Module):
         return responses
     
     def recaptcha2_enterprise_proxyless(self,
-             website_url: str,  # Address of a webpage with Google ReCaptcha
+             website_url: str,  # Address of a webpage with Google ReCaptcha Enterprise
              website_key: str,  # Recaptcha website key.
              enterprisePayload: str = None, # Some implementations of the reCAPTCHA Enterprise widget may contain additional parameters that are passed to the “grecaptcha.enterprise.render” method along with the sitekey.
              api_key:str = None,    # API key from https://api.capmonster.cloud/
@@ -277,7 +277,7 @@ class Captcha(c.Module):
         return responses
     
     def recaptcha2_enterprise(self,
-             website_url: str,  # Address of a webpage with Google ReCaptcha
+             website_url: str,  # Address of a webpage with Google ReCaptcha Enterprise
              website_key: str,  # Recaptcha website key.
              proxyType: str,    # Type of the proxy (http, https, socks3, socks5),
              proxyAddress: str, # Proxy IP address IPv4/IPv6. (not allowed to use hostnames, transparent proxies, local networks)
@@ -325,6 +325,27 @@ class Captcha(c.Module):
         request = RecaptchaV3ProxylessRequest(
             websiteUrl=website_url, # "https://lessons.zennolab.com/captchas/recaptcha/v3.php?level=beta"
             websiteKey=website_key, # "6Le0xVgUAAAAAIt20XEB4rVhYOODgTl00d8juDob"
+        )
+
+        responses = asyncio.run(_solve_captcha())
+        return responses
+    
+    def turnstile_proxyless(self,
+             website_url: str,  # Address of a webpage with Google ReCaptcha Enterprise
+             website_key: str,  # Turnstile key
+             api_key:str = None,    # API key from https://api.capmonster.cloud/
+    ) -> str:
+        api_key = api_key if api_key != None else self.api_key
+
+        client_options = ClientOptions(api_key=api_key)
+        cap_monster_client = CapMonsterClient(options=client_options)
+
+        async def _solve_captcha():
+            return await cap_monster_client.solve_captcha(request)
+        
+        request = TurnstileProxylessRequest(
+            websiteUrl=website_url, # "http://tsmanaged.zlsupport.com"
+            websiteKey=website_key, # "0x4AAAAAAABUYP0XeMJF0xoy"
         )
 
         responses = asyncio.run(_solve_captcha())
