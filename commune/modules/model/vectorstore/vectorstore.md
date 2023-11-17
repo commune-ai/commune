382637c97235bc9c900d20b173c5fd294ec5c263
ggsofter <var196@outlook.com>
2023-11-17 11:57:14 -0800

    add vector store module

--- commune/modules/model/vectorstore/vectorstore.md
+++ commune/modules/model/vectorstore/vectorstore.md
@@ -34,3 +34,15 @@ c model.vectorstore search api_key=... path=... query=...
 ```python
 c model.vectorstore search api_key="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" path="state_of_the_union.txt" query="What did the president say about Ketanji"
 ```
+
+Add embedding sentence
+#### Bash
+```bash
+c model.vectorstore add_sentence sentence="fam is owner of commune."
+```
+
+Get sentence by prompting
+#### Bash
+```bash
+c model.vectorstore prompt query="Who is fam?"
+```
--- commune/modules/model/vectorstore/vectorstore.py
+++ commune/modules/model/vectorstore/vectorstore.py
@@ -6,6 +6,10 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from typing import Union, List, Any, Dict
+import sqlite3
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
 
 __import__('pysqlite3')
 import sys
@@ -17,6 +21,74 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
+
+class DatabaseManager:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS embeddings
+                     (sentence text, embedding text)''')
+
+    def insert_embedding(self, sentence, embedding):
+        self.cursor.execute("INSERT INTO embeddings VALUES (?,?)", (sentence, str(list(embedding))))
+        self.conn.commit()
+
+    def update_embedding(self, sentence, new_embedding):
+        self.cursor.execute("UPDATE embeddings SET embedding = ? WHERE sentence = ?", (str(list(new_embedding)), sentence))
+        self.conn.commit()
+
+    def delete_embedding(self, sentence):
+        self.cursor.execute("DELETE FROM embeddings WHERE sentence = ?", (sentence,))
+        self.conn.commit()
+
+    def fetch_embedding(self, sentence):
+        self.cursor.execute("SELECT * FROM embeddings WHERE sentence = ?", (sentence,))
+        return self.cursor.fetchone()
+
+    def fetch_all_embeddings(self):
+        self.cursor.execute('SELECT * FROM embeddings')
+        return self.cursor.fetchall()
+
+class EmbeddingModel:
+    def __init__(self, model_name):
+        self.model = SentenceTransformer(model_name)
+
+    def get_embedding(self, sentence):
+        return self.model.encode([sentence])[0]
+
+class SentenceManager:
+    def __init__(self, db_manager, embedding_model):
+        self.db_manager = db_manager
+        self.embedding_model = embedding_model
+
+    def add_sentence(self, sentence):
+        embedding = self.embedding_model.get_embedding(sentence)
+        self.db_manager.insert_embedding(sentence, embedding)
+        return {'msg': f"Sentence is added to local vector store.", 'success': True}
+
+    def update_sentence(self, sentence):
+        new_embedding = self.embedding_model.get_embedding(sentence)
+        self.db_manager.update_embedding(sentence, new_embedding)
+        return {'msg': f"Sentence is updated from local vector store.", 'success': True}
+
+    def delete_sentence(self, sentence):
+        self.db_manager.delete_embedding(sentence)
+        return {'msg': f"Sentence is deleted from local vector store.", 'success': True}
+
+    def get_sentence_embedding(self, sentence):
+        return self.db_manager.fetch_embedding(sentence)
+
+    def prompt(self, query):
+        query_embedding = self.embedding_model.get_embedding(query)
+        rows = self.db_manager.fetch_all_embeddings()
+        return max(rows, key=lambda row: cosine_similarity([query_embedding], [np.array(eval(row[1]))]))[0]
+
+db_manager = DatabaseManager('embeddings.db')
+embedding_model = EmbeddingModel('all-MiniLM-L6-v2')
+sentence_manager = SentenceManager(db_manager, embedding_model)
 
 class ModelVectorstore(c.Module):
     def __init__(self, config = None, **kwargs):
@@ -88,3 +160,10 @@ class ModelVectorstore(c.Module):
     def save_api_keys(cls, api_keys:List[str]):
         cls.put('api_keys', api_keys)
         return {'msg': f"Saved API Keys", 'success': True}
+
+    def add_sentence(self, sentence):
+        return sentence_manager.add_sentence(sentence)
+
+    def prompt(self, query):
+        result = sentence_manager.prompt(query)
+        return {'result': result, 'success': True}
