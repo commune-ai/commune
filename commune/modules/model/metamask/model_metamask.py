b336b501c28a77b43e8fdc2d6788716517b5b55e
bluepadu <padum3955@gmail.com>
2023-12-07 14:50:27 -0500

    Add: Readme

--- commune/modules/model/metamask/model_metamask.py
+++ commune/modules/model/metamask/model_metamask.py
@@ -1,23 +1,12 @@
+import subprocess
 import commune as c
-import streamlit as st
-from components.metamask import metamask_connection
-from components.transaction import display_transaction_ui
-from utils import format_eth_address
+import os
 
 class ModelMetamask(c.Module):
     def __init__(self, config = None, **kwargs):
         self.set_config(config, kwargs=kwargs)
 
-    def call():
-        main()
-        
-def main():
-    st.title('Ethereum Transaction App with MetaMask')
-
-    # MetaMask Connection
-    if metamask_connection():
-        address = st.session_state.get('address')
-        st.write(f"Connected Ethereum Address: {format_eth_address(address)}")
-
-        # Display Transaction UI
-        display_transaction_ui()
+    def run():
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        app_path = os.path.join(dir_path, 'app.py')
+        subprocess.run(['streamlit', 'run', app_path])
