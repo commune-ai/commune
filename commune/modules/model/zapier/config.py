2992edbcde72cdd0cfcc5516152f5c04a619c9bc
shahjab <shahjab323@gmail.com>
2024-01-18 22:15:28 +0100

    zap: com-transfer tracker

--- commune/modules/model/zapier/config.py
+++ commune/modules/model/zapier/config.py
@@ -1,13 +1,13 @@
 # Discord Channel IDs
-mail_channel_id = ''
-
+mail_channel_id = 965512433458356226 
 # host:port
-imap_host = ''
+imap_host = 'imap.gmail.com'
 imap_port = 993
-
+smtp_host = 'smtp.gmail.com:465'
 # AUTH for both imap & smtp
-userEmail = ''
-password = ''
-
+userEmail = 'jackveustar1228@gmail.com'
+password = 'phuwgsiwxajsbocz'
+#  
+from_mail = 'jackveustar1228@gmail.com'
 # Discord Bot Token
-token = ''
+token = 'MTE3NzYwNDI2NTM3MjU0OTEzMA.GZm_Zr.JtViWqH7pMXJiYNquQlLSqGtzT-PwxsnJ4aQkQ'
