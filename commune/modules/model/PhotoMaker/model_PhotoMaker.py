c06957c7577f57b9cc9189b14657c261fe336049
c-spider <davidleiva4999@gmail.com>
2024-02-13 02:17:08 +0000

    PhotoMaker: hot fix

--- commune/modules/model/PhotoMaker/model_PhotoMaker.py
+++ commune/modules/model/PhotoMaker/model_PhotoMaker.py
@@ -11,51 +11,52 @@ image_encoder_path = "models/image_encoder/"
 ip_ckpt = "models/ip-adapter-full-face_sd15.bin"
 device = "cuda"
 
-noise_scheduler = DDIMScheduler(
-    num_train_timesteps=1000,
-    beta_start=0.00085,
-    beta_end=0.012,
-    beta_schedule="scaled_linear",
-    clip_sample=False,
-    set_alpha_to_one=False,
-    steps_offset=1,
-)
-vae = AutoencoderKL.from_pretrained(vae_model_path).to(dtype=torch.float16)
 
-# load SD pipeline
-pipe = StableDiffusionPipeline.from_pretrained(
-    base_model_path,
-    torch_dtype=torch.float16,
-    scheduler=noise_scheduler,
-    vae=vae,
-    feature_extractor=None,
-    safety_checker=None
-)
-
-ip_model = IPAdapterFull(pipe, image_encoder_path, ip_ckpt, device, num_tokens=257)
 
 
 class Demo(c.Module):
     def __init__(self, a=1, b=2):
         self.set_config(kwargs=locals())
+                
+        self.noise_scheduler = DDIMScheduler(
+            num_train_timesteps=1000,
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            clip_sample=False,
+            set_alpha_to_one=False,
+            steps_offset=1,
+        )
+        self.vae = AutoencoderKL.from_pretrained(vae_model_path).to(dtype=torch.float16)
+
+        # load SD pipeline
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            base_model_path,
+            torch_dtype=torch.float16,
+            scheduler=self.noise_scheduler,
+            vae=self.vae,
+            feature_extractor=None,
+            safety_checker=None
+        )
+        self.ip_model = IPAdapterFull(self.pipe, image_encoder_path, ip_ckpt, device, num_tokens=257)
 
     def call(self, x:int = 1, y:int = 2) -> int:
         c.print(self.config)
         c.print(self.config, 'This is the config, it is a Munch object')
         return x + y
     
-    def generate(self, imgUrl="assets/images/ai_face.png", promptText="") :      
+    def generate(self, imgUrl="assets/images/ai_face.png", promptText="wearing sunglases") :      
         # read image prompt
         image = Image.open(imgUrl)
         image.resize((256, 256))
 
         # use face as image prompt
-        images = ip_model.generate(
-            pil_image=image, num_samples=4, prompt="A photo of a girl wearing a black dress, holding red roses in hand, upper body, behind is the Eiffel Tower",
+        images = self.ip_model.generate(
+            pil_image=image, num_samples=4, prompt=promptText,
             scale=0.7, width=512, height=512, num_inference_steps=50, seed=42)
         
         for i, image in enumerate(images):
             image.save(f"{i}.png")
 
         c.print("File created. 0.png, 1.png, 2.png, 3.png")
-        return
+        return images
