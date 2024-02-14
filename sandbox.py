3872eb89e2b1262b63cbc055b19eb8becb313897
salvivona <you@example.com>
2024-02-14 15:27:39 -0500

    added more nodes

--- sandbox.py
+++ sandbox.py
@@ -1,35 +1,18 @@
 import argparse
 import commune as c
 
-# Set up command-line argument parsing
-parser = argparse.ArgumentParser(description='Transfer all balance to a specific address.')
-parser.add_argument('--address', type=str, help='The address to transfer to.')
-parser.add_argument('--min', type=float, default=0.5, help='Min stake amount to left. Default is 0.5.')
-parser.add_argument('--timeout', type=float, default=10, help='The timeout for the task. Default is 10.')
-
-# Parse command-line arguments
-args = parser.parse_args()
-
-def unstake_fn(address):
-    # Unstake all balance from the wallet
-    staked = c.get_stake(address)
-    print(f"Staked amount is {staked}")
-    if (staked > 0):
-        c.unstake(staked, address)
-    
-    # Transfer all balance to the specific address
-    balance = c.get_balance(address)
-    print(f"Total balance is {balance}")
-
-    if (balance > args.min):
-        c.transfer(args.address, balance - args.min, address)
+subspace = c.module('subspace')
+config = subspace.config(to_munch=False)
 
 
-for address in c.keys():
-    c.print(f"Unstaking and transferring all balance from {address} to {args.address}...")
-    futures = c.submit(unstake_fn, address, timeout=args.timeout)
+urls ={'ws': [], 'http': []}
+for i in range(1,5):
+    name = f"commune-api-node-{i}.communeai.net"
+    urls['ws'].append(f"wss://{name}/")
+    urls['http'].append(f"https://{name}/")
 
-for future in c.as_completed(futures, timeout=args.timeout): 
-    result = future.result()
-    c.print(result)    
+
+config['urls']['commies'] = urls
+
+subspace.save_config(config)
 
