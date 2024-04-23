5fc31367a81c95a1b6e52f06fe4197a169716f2f
latentvector <you@example.com>
2024-04-23 12:26:34 -0400

    cleaned

--- modules/demo/demo.py
+++ modules/demo/demo.py
@@ -1,8 +1,7 @@
 import commune as c
-from .utils import a
 
 class Demo(c.Module):
-    def __init__(self, a=a):
+    def __init__(self, a=1):
         self.a = a
     def call(self, b = 1):
         return self.a + b
--- omega/omega.py
+++
@@ -1,9 +0,0 @@
-import commune as c
-
-class Omega(c.Module):
-    def __init__(self, a=1):
-        self.a = a
-    def call(self, b = 1):
-        return self.a + b
-
-
