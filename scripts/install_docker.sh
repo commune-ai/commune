1d1a14457b4376d41582f6cd19f82a6cd3c1cdab
latentvector <you@example.com>
2024-04-25 21:32:34 -0400

    remote changes and script improvement to docker

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
-# Update the package database and install necessary dependencies
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add the Docker repository to the package sources list
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Update the package database again to include the Docker packages
-sudo apt-get update
-
 # Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Verify that Docker has been installed correctly
-sudo docker run hello-world
+groupadd docker
+usermod -aG docker $USER
+chmod 666 /var/run/docker.sock
+
+
