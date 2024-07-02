725da9cabb21c0dc61be67d8daaad7be33450fe7
latentvector <you@example.com>
2024-07-02 00:03:47 -0400

    removed modules au

--- modules/model/finetune/finetune.md
+++
@@ -1,6 +0,0 @@
-## Fine Tuning module
-
-#### Get prompt
-```
-c model.finetune prompt "What is the meaning of the world?"
-```
--- modules/model/metamask/utils.py
+++
@@ -1,3 +0,0 @@
-def format_eth_address(address):
-    """Format Ethereum address for display."""
-    return address[:6] + '...' + address[-4:]
