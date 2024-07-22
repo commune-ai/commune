bdabdaf1d79b67e4ac2021d22c1004ab09be84b1
latentvector <you@example.com>
2024-07-21 21:43:53 -0400

    fixed app leaderboard

--- arena/app.py
+++ arena/app.py
@@ -6,11 +6,15 @@ import streamlit as st
 import plotly.express as px
 import datetime
 
+
+
 class App(c.Module):
     def __init__(self, 
                  model = 'model.openrouter', 
                  score_module = 'arena.score'):
         self.model = c.module(model)()
+        # set wide
+        st.set_page_config(layout="wide")
         self.score_model = c.module(score_module)()
     def signin(self):
         
@@ -88,38 +92,64 @@ class App(c.Module):
 
     def arena(self):
         # c.load_style()
-        model = st.selectbox('Select a model', self.score_model.models())
-        text = st.text_area('Red Team Prompt')
+
+              
         cols = st.columns([1,1])
+        with cols[0]:
+            model = st.selectbox('Select a model', self.score_model.models())
+            _cols = st.columns([1,1])
+            temperature = _cols[0].slider('Temperature', 0.0, 1.0, 0.5)
+            max_tokens = _cols[1].number_input('Max Tokens', 1, 10000, 10000)
+            text = st.text_area('Red Team Prompt')
+            _cols = st.columns([1,1])
 
-        submit_attack = cols[0].button('Submit Attack')
-        cancel_attack = cols[1].button('Cancel Attack')
-        attack_model = submit_attack and not cancel_attack
-        if attack_model :
-            model_response_generator = self.model.forward(text, model=model, stream=True)
+            submit_attack = _cols[0].button('Submit Attack')
+            cancel_attack = _cols[1].button('Cancel Attack')
+            attack_model = submit_attack and not cancel_attack
 
-            model_dict =  {'response': ''}
-            def generator_stream(generator, model_dict=None):
-                for token in generator:
-                    if model_dict != None:
-                        model_dict['response'] += token
-                    yield token
-                
-                
-            st.write_stream(generator_stream(model_response_generator, model_dict))
-            model_response = model_dict['response']
+            
+        with cols[1]:
+            if attack_model :
+                model_response_generator = self.model.forward(text, model=model, stream=True, max_tokens=max_tokens, temperature=temperature)
 
-            result = self.score_model.score(model_response)# dict where mean is the score
-            result['prompt'] = text
-            result['response'] = model_response
-            result['model'] = model
-            result['address'] = self.key.ss58_address
-            self.save_result(result)
+                model_dict =  {'response': ''}
+                def generator_stream(generator, model_dict=None):
+                    for token in generator:
+                        if model_dict != None:
+                            model_dict['response'] += token
+                        yield token
+                    
+                    
+                with st.status(f"Getting Response", expanded=True):
+                    st.write_stream(generator_stream(model_response_generator, model_dict))
+                    model_response = model_dict['response']
 
-            with st.status(f"Jailbreak Score ({result['mean']})", expanded=False):
-                result.pop('response')
-                result.pop('prompt')
-                st.write(result)
+                    result = self.score_model.score(model_response)# dict where mean is the score
+                    result['prompt'] = text
+                    result['response'] = model_response
+                    result['model'] = model
+                    result['temperature'] = temperature
+                    result['max_tokens'] = max_tokens
+                    result['address'] = self.key.ss58_address
+                    self.save_result(result)
+
+                with st.status(f"Jailbreak Score ({result['mean']})", expanded=True):
+                    result.pop('response')
+                    result.pop('prompt')
+                    model2score = result['model2score']
+                    # write a plot linear polar plot
+                    scores = list(model2score.values())
+                    models = list(model2score.keys())
+    
+                    fig = px.line_polar(r=scores, theta=models, line_close=True)
+                    # black background on the radial plot with transparent background
+                    # rgba that is really night vibes and cyber punk
+                    # paper_bgcolor='rgba(0,256,5,1)'
+                    fig.update_layout(
+                                      polar=dict(radialaxis=dict(visible=True, range=[0, 1])))
+                    st.plotly_chart(fig)
+                # mak
+
 
     def save_result(self, response):
         model = response['model']
@@ -128,26 +158,35 @@ class App(c.Module):
         path =  f'history/{model}/{address}/{c.time()}.json'
         self.put_json(path, response)
 
-    def my_history(self, columns=['mean', 'timestamp', 'model', 'address'], sort_by='timestamp', ascending=False, model=None):
+    def my_history(self, 
+                   columns=['time', 'model', 'prompt', 'response', 'score', 'temperature', 'models', 'scores'], 
+                   sort_by='time',
+                     ascending=False,
+                       model=None):
         df = c.df(self.get_history(model=model))
-        if len(df) > 0:
-            df = df[columns].sort_values(sort_by, ascending=ascending)
-        else:
-            st.write('No history found')
+        if len(df) == 0:
+            st.error('No history found, enter the arena')
             return df
         # convert timestmap to human readable
+        df['models'] = df['model2score'].apply(lambda x: list(x.keys() if isinstance(x, dict) else []))
+        df['scores'] = df['model2score'].apply(lambda x: list(x.values() if isinstance(x, dict) else []))
+        df['score'] = df['mean']
         df['time'] = df['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%m-%d %H:%M'))
-        del df['timestamp']
-        del df['address']
+        df = df[columns].sort_values(sort_by, ascending=ascending)
         df = df.set_index('time')
         df.sort_index(inplace=True)
         return df
+    
+
+    def my_stats(self):
+        with st.expander('History', expanded=False):
+            st.write(self.my_history())
 
     def leaderboard(self, 
               columns=['mean', 'timestamp', 'model', 'address'],
               group_by = ['address', 'model'], 
               sort_by='mean', ascending=False, model=None):
-        cols = st.columns([4,1])
+        cols = st.columns([1,1])
         for i in range(2):
             cols[0].write('\n')
 
@@ -156,55 +195,56 @@ class App(c.Module):
         if len(df) == 0:
             st.error('No history found, enter the arena')
             return df
-        
+
         # PROCESS THE DATA
         df = df[columns].sort_values(sort_by, ascending=ascending)
         # convert timestmap to human readable
         df['time'] = df['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
         del df['timestamp']
-
         # group based on address
 
-
-        st.write('Top Jailbreakers')
-        user_df = df
-        # select a model
-        models = ['ALL'] + list(user_df['model'].unique())
+        models = ['ALL'] + list(df['model'].unique())
         model = st.selectbox('Select a models', models, 0)
+        # select a model
         if model != 'ALL':
-            user_df = user_df[user_df['model'] == model]
-        user_df = user_df.groupby('address')['mean'].agg(['mean', 'std', 'count']).reset_index()
-        user_df = user_df.sort_values('mean', ascending=False)
-        st.write(user_df)
-
-        st.write('Least Jailbroken Models') 
-        model_df = df.groupby('model')['mean'].agg(['mean', 'std', 'count']).reset_index()
-        model_df = model_df.sort_values('mean', ascending=False)
-        st.write(model_df)
+            df = df[df['model'] == model]
+        with cols[0]:
+            st.write('#### Top Jailbreakers')
+            user_df = df.groupby('address')['mean'].agg(['mean', 'std', 'count']).reset_index()
+            user_df = user_df.sort_values('mean', ascending=False)
+            st.write(user_df)
+        with cols[1]:
+            st.write('#### Most Robust (Least Jailbroken) Models') 
+            model_df = df.groupby('model')['mean'].agg(['mean', 'std', 'count']).reset_index()
+            model_df = model_df.sort_values('mean', ascending=False)
+            st.write(model_df)
 
     def sidebar(self, sidebar=True):
-    
         if sidebar:
             with st.sidebar:
                 return self.sidebar(sidebar=False)
-        st.write('# arena')
+            
+
+        self.top_header()
         self.signin()
         teams = ['red', 'blue']
         # side by side radio buttons
         self.team = st.radio('Select a team', teams, index=0)
+        with st.expander('Game Rules'):
+            self.help()
         
 
     def top_header(self):
         # have a random image
-        st.write(f'# TEAM {self.team.upper()} ') 
+        return st.write('# JAILBREAK ARENA')
 
 
     def app(self):
         self.sidebar()
         self.load_style()
-        self.top_header()
-        fns = [ f'arena', 'leaderboard', 'help']
-        tab_names = ['Attack', 'Leaderboard', 'Mission Details']
+
+        fns = [ f'arena', 'leaderboard']
+        tab_names = ['Attack', 'Leaderboard']
         tabs = st.tabs(tab_names)
         for i, fn in enumerate(fns):
             with tabs[i]:
