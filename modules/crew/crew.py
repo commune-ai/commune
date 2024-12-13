ea6ca76571324402adeef26540b38d95f9cbe048
tentru <tensortrust@gmail.com>
2024-12-13 09:46:05 -0700

    wrap agent with commune module

--- modules/crew/crew.py
+++ modules/crew/crew.py
@@ -1,12 +1,12 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
+import commune as c
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
-class CrewAgent():
+class CrewAgent(c.Module):
 	"""CrewAgent crew"""
 
 	# Learn more about YAML configuration files here:
