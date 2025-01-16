f76452788c359674c2e33361f71c5b2b708ec159
drunest <6984754+drunest@users.noreply.github.com>
2025-01-15 18:58:59 -0600

    chore: use modular approach with folder

--- commune/__init__.py
+++ commune/__init__.py
@@ -1,13 +1,14 @@
+from .module import Module  # the module module
 
+M = c = Block = Agent = Module  # alias c.Module as c.Block, c.Lego, c.M
+from .vali.vali import Vali  # the vali module
+from .server.server import Server  # the server module
+from .client import Client  # the client module
+from .key.key import Key  # the key module
 
-
-from .module import Module # the module module
-M = c = Block = Agent  = Module # alias c.Module as c.Block, c.Lego, c.M
-from .vali import Vali # the vali module
-from .server import Server # the server module
-from .client import Client # the client module
-from .key.key import Key # the key module
 # set the module functions as globalsw
 c.add_to_globals(globals())
-key = c.get_key # override key function with file key in commune/key.py TODO: remove this line with a better solution
+key = (
+    c.get_key
+)  # override key function with file key in commune/key.py TODO: remove this line with a better solution
 network = c.network
