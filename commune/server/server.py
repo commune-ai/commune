572afe98ac6373c0f60d3b65d4a56ebf75b39f2d
sal <homie@sals-MacBook-Pro.local>
2025-01-23 17:41:08 -0500

    server refactor

--- commune/server/server.py
+++ commune/server/server.py
@@ -38,7 +38,6 @@ class Server(c.Module):
         serializer: str = 'serializer', # the serializer used for the data
         middleware: Optional[callable] = None, # the middleware for the server
         run_app = True, # if the server should be run
-
         ) -> 'Server':
         self.add_network(network)  
         self.serializer = c.module(serializer)()
@@ -523,23 +522,8 @@ class Server(c.Module):
 
     @classmethod
     def server_exists(cls, name):
-        return cls.pm.exists(name) and cls.net['local'].namespace().get(name, None) != None
+        return cls.net['local'].namespace().get(name, None) != None
 
-    @staticmethod
-    def test_serving_with_different_key(module = 'module', timeout=10):
-        tag = 'test_serving_with_different_key'
-        key_name = module + '::'+ tag
-        module_name =  module + '::'+ tag + '_b' 
-        c.print(c.serve(module_name, key=key_name))
-        key = c.get_key(key_name)
-        c.sleep(2)
-        info = c.call(f'{module_name}/info', timeout=2)
-        assert info.get('key', None) == key.ss58_address , f" {info}"
-        c.kill(module_name)
-        c.rm_key(key_name)
-        assert not c.key_exists(key_name)
-        assert not c.server_exists(module_name)
-        return {'success': True, 'msg': 'server test passed'}
 if __name__ == '__main__':
     Server.run()
 
