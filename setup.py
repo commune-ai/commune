4c649128e53992682d53b3ea026b51b366609b3e
Luca Vivona <lucavivona01@gmail.com>
2025-01-31 17:02:18 -0500

    chore: simple setup.py/pyproject.tom build

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
-
 from setuptools import setup, find_packages
-from os import path
-from io import open
-from pkg_resources import parse_requirements
-here = path.abspath(path.dirname(__file__))
-repo = 'commune'  # name of the package is assumed to be the name of the directory
-with open(f'{here}/README.md', encoding='utf-8') as f:
-    long_description = f.read()
-with open(f'{here}/requirements.txt') as requirements_file:
-    install_requires = [str(requirement) for requirement in parse_requirements(requirements_file)]
+
 setup(
-    name=repo,
-    version='1.0.0',
-    description='a way for connecting and verifying tools for the global toolbox',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/commune-ai/commune',
-    author='physics',
+    name="commune",
+    version="0.0.1",
+    description="Global toolbox that allows you to connect and verify your tools",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Commune AI Organization",
+    author_email="developer@communeai.org",
     packages=find_packages(),
     include_package_data=True,
-    author_email='',
-    license='IDGAF License, Do What You Want, I wont sue you',
-    install_requires=install_requires,
-    entry_points={'console_scripts': [f'c={repo}.cli:main']},
-    classifiers=['FUCK SHIT UP'], 
-    python_requires='>=3.8')
+    package_data={
+        'commune': ['*.py'],
+    },
+    entry_points={
+        'console_scripts': [
+            'commune=commune.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">3.9",
+)
